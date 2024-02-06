e8c11890e3aafedcc967c89485dab74d3439e102
신준혁 <oridori2705@naver.com>
2024-02-06 11:36:32 +0900

    fix: 스타일 문제 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import Image from "next/image";
-import Icon from "./_component/common/Icon";
-
 export default function Home() {
-  return (
-    <main className="">
-      <Icon id="gavel" fill="white" />
-    </main>
-  );
+  return <main className=""></main>;
 }
