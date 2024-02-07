72c9101edc601f101df5627708944c0482c89056
imssein <hwinnie131@gmail.com>
2024-02-07 12:03:37 +0900

    feat: props, className 타입 추가

--- src/app/_component/common/Badge/index.tsx
+++ src/app/_component/common/Badge/index.tsx
@@ -1,26 +1,40 @@
 import { cn } from "@/utils/cn";
-import { VariantProps, cva } from "class-variance-authority";
+import { VariantProps } from "class-variance-authority";
+import { BadgeVariants } from "./Badge.variants";
 
 interface BadgeProps extends VariantProps<typeof BadgeVariants> {
-  children: React.ReactElement;
-  content?: React.ReactElement;
+  children: React.ReactNode;
+  content?: React.ReactNode;
   maxCount?: number;
+  className?: string;
 }
 
 const Badge = ({
   position,
   size,
+  badgeType,
+  children,
+  content,
+  maxCount,
+  className,
+  ...props
 }: BadgeProps) => {
+  const badgeContent =
+    typeof content === "number" && maxCount && content > maxCount
+      ? `${maxCount}+`
+      : content;
+
   return (
-    <div className="relative inline-block">
+    <div
+      className="relative inline-block"
+      {...props}>
       {children}
-      <span className={cn(BadgeVariants({ position, size, badgeType }))}>
-        {typeof content === "number" && maxCount && content > maxCount
-          ? `${maxCount}+`
-          : content}
+      <span
+        className={cn(BadgeVariants({ position, size, badgeType }), className)}>
+        {badgeContent}
       </span>
     </div>
   );
-}
+};
 
 export default Badge;
