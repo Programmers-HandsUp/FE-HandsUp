cfbd695b120e7cadfc5e6c04f7b804e5d8d0cd43
세인 <93479475+imssein@users.noreply.github.com>
2024-02-07 16:22:41 +0900

    feat: Badge 컴포넌트 구현 (#19)
    * feat: badge 컴포넌트 구현
    
    * fix: husky 설정 수정
    
    * refactor: variants 추상화
    
    * refactor: 새롭게 정한 컨벤션에 따른 수정(타입명, 함수표현식)
    
    * feat: props, className 타입 추가

---
+++ src/app/_component/common/Badge/index.tsx
@@ -0,0 +1,40 @@
+import { cn } from "@/utils/cn";
+import { VariantProps } from "class-variance-authority";
+import { BadgeVariants } from "./Badge.variants";
+
+interface BadgeProps extends VariantProps<typeof BadgeVariants> {
+  children: React.ReactNode;
+  content?: React.ReactNode;
+  maxCount?: number;
+  className?: string;
+}
+
+const Badge = ({
+  position,
+  size,
+  badgeType,
+  children,
+  content,
+  maxCount,
+  className,
+  ...props
+}: BadgeProps) => {
+  const badgeContent =
+    typeof content === "number" && maxCount && content > maxCount
+      ? `${maxCount}+`
+      : content;
+
+  return (
+    <div
+      className="relative inline-block"
+      {...props}>
+      {children}
+      <span
+        className={cn(BadgeVariants({ position, size, badgeType }), className)}>
+        {badgeContent}
+      </span>
+    </div>
+  );
+};
+
+export default Badge;
