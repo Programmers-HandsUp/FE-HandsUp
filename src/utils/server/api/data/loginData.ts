a818ac7ae20eb5df57958ef9dea272c26cd09cab
JangGwon <tlsakch510@naver.com>
2024-02-10 21:51:41 +0900

    feat : Msw - 로그인 데이터 추가

---
+++ src/utils/server/api/data/loginData.ts
@@ -0,0 +1,15 @@
+interface LoginFormType {
+  id: string;
+  passWord: string;
+}
+
+export const userAuthData: LoginFormType[] = [
+  {
+    id: "abcd1234",
+    passWord: "*abcd1234",
+  },
+  {
+    id: "abab1234",
+    passWord: "*abab1234",
+  },
+];
