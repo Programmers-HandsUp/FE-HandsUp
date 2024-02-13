87f8a0c2ff11de3a28afbff374fe631a9b037af9
JangGwon <40955023+JuJangGwon@users.noreply.github.com>
2024-02-14 01:24:54 +0900

    Settings : MSW 도입 및 auth 관련, 경매 포스트 관련 Mocking APi 구현 (#40)
    * feat : input 컴포넌트 뼈대 구현
    
    * refactor : 기존 input 컴포넌트 합성컴포넌트패턴 형식으로 변경
    
    * feat : mockServiceWorker 생성
    
    * settings : Msw - 포스트 불러오기 관련 mocking 데이터 설정
    
    * feat : Msw - 로그인 데이터 추가
    
    * feat : msw -  상품 올리기 mocking postApi 구현
    
    * feat : msw - 로그인, 회원 가입 모킹데이터 추가
    
    * refactor : 콘솔로그 제거 및 postlist 관련 api 주소 수정
    
    * refactor : PR 전 테스트 코드 제거
    
    * refactor : mocking Api 관련 파일명 수정 및 핸들러 연결
    
    * rename : msw - 경매생성, 불러오기 api 경로, 파일명 수정
    
    * refactor : utils/server -> utils/MockServer로 폴더명 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+if (process.env.NODE_ENV === "development") {
+  import("@/utils/server");
+}
+
 export default function Home() {
   return <main className=""></main>;
 }
---
+++ src/utils/mockServer/api/data/authData.ts
@@ -0,0 +1,12 @@
+import { authDataType } from "../types";
+
+export const userAuthData: authDataType[] = [
+  {
+    id: "abcd1234",
+    passWord: "*abcd1234",
+  },
+  {
+    id: "abab1234",
+    passWord: "*abab1234",
+  },
+];
