3dcae538ac825f903271b2fb92aac1b5e40a36de
세인 <93479475+imssein@users.noreply.github.com>
2024-02-13 12:08:25 +0900

    feat: Tooltip 컴포넌트 구현 (#32)
    * feat: interaction hook 구현
    
    * feat: Tooltip 컴포넌트 구현
    
    * feat: className prop 추가
    
    * refactor: useCallback대신 일반 함수로 변경

---
+++ src/app/_component/common/Tooltip/index.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { cn } from "@/utils/cn";
+import useInteraction, { EventType } from "./useInteraction";
+import {
+  MainPositionVariants,
+  PositionVariants,
+  ArrowPositionVariants
+} from "./Tooltip.variants";
+import React, { PropsWithChildren } from "react";
+import { VariantProps } from "class-variance-authority";
+
+interface TooltipProps extends VariantProps<typeof MainPositionVariants> {
+  eventType: EventType;
+  showArrow?: boolean;
+  content: React.ReactNode;
+  className?: string;
+}
+
+const Tooltip = ({
+  eventType,
+  position,
+  showArrow = true,
+  content,
+  children,
+  className,
+  ...props
+}: PropsWithChildren<TooltipProps>) => {
+  const { ref, isInteracted } = useInteraction(eventType);
+  return (
+    <div className="cursor-pointer">
+      <div
+        className="relative"
+        ref={ref}>
+        {children}
+        {isInteracted && (
+          <div
+            className={cn(MainPositionVariants({ position }))}
+            {...props}>
+            <span
+              className={cn(PositionVariants({ position }))}
+              {...props}>
+              {content}
+            </span>
+            {showArrow && (
+              <span
+                className={cn(ArrowPositionVariants({ position }))}
+                {...props}></span>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Tooltip;
