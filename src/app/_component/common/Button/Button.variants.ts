aa8ad3d42d3f9481ef94646296079eb2074f3dda
Jiho Heo <104545906+hj9118@users.noreply.github.com>
2024-02-15 08:55:40 +0900

    feat: Button 컴포넌트 구현 (#42)
    * feat: Button 컴포넌트 구현
    
    * design: text 색상 수정
    
    * refactor: variants 이동
    
    * modify: EOL 수정
    
    * modify: 화살표 함수로 수정
    
    * design: darkGray 옵션 추가
    
    * refactor: props에서 children으로 수정
    
    * feat: 로딩기능 추가

---
+++ src/app/_component/common/Button/Button.variants.ts
@@ -0,0 +1,29 @@
+import { cva } from "class-variance-authority";
+
+export const ButtonVariants = cva(
+  "inline-flex items-center justify-center",
+  {
+    variants: {
+      size: {
+        small: "px-2 py-1 text-sm",
+        medium: "px-4 py-2",
+        large: "px-6 py-3 text-lg",
+      },
+      rounded: {
+        none: "rounded-md",
+        full: "rounded-full",
+      },
+      color: {
+        primary: "bg-[#96E4FF] text-black hover:bg-[#96E4FF]/50",
+        gray: "bg-[#D9D9D9] text-black hover:bg-[#D9D9D9]/50",
+        darkGray: "bg-[#62696B] text-black hover:bg-[#62696B]/50",
+        yellow: "bg-[#FFE604] text-black hover:bg-[#FFE604]/50",
+      }
+    },
+    defaultVariants: {
+      size: "medium",
+      rounded: "none",
+      color: "primary"
+    }
+  }
+);
