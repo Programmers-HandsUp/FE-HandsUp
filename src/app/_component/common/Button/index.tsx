963a4163611cad3c3e425837626e1a7eea41d9fd
hj9118 <hwl9118@gmail.com>
2024-02-14 23:39:40 +0900

    feat: 로딩기능 추가

--- src/app/_component/common/Button/index.tsx
+++ src/app/_component/common/Button/index.tsx
@@ -8,11 +8,17 @@ import { VariantProps } from "class-variance-authority";
 interface Props extends VariantProps<typeof ButtonVariants>, Omit<ButtonHTMLAttributes<HTMLButtonElement>, "type"> {
   icon?: IconName;
   color: "primary" | "gray" | "darkGray" | "yellow";
+  loading?: boolean;
   children: ReactNode;
 }
 
-const Button = ({ size, rounded, icon, color, children, ...props }: Props) => {
+const Button = ({ size, rounded, icon, color, loading, children, ...props }: Props) => {
   return (
+    loading ? 
+    <button className={cn(ButtonVariants({ size, rounded, color }))} {...props} disabled>
+      Loading
+    </button>
+    :
     <button className={cn(ButtonVariants({ size, rounded, color }))} {...props}>
       {icon && <Icon id={icon} size={24} className="mr-2" />}
       {children}
