b356ead9b33c20ae9fd1b2d10650d0c686fdb3c2
신준혁 <90139306+oridori2705@users.noreply.github.com>
2024-02-14 16:58:11 +0900

    feat: Navigation 구현 (#39)
    * feat: 네비게이션 구현 및 아이콘 추가
    
    * refactor: Toast 컴포넌트 위치 수정 및 크기 수정
    
    * fix: public 경로 문제 해결
    
    * modify: 로그인 유무 flow 수정
    
    * fix: 싱글톤 패턴 버그 수정
    
    * refactor: 북마크 이동버튼 추가 및 Link에 onClick 추가

---
+++ src/app/_component/common/Navigation/LoginLink.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Link from "next/link";
+import { MouseEvent, ReactNode } from "react";
+import Toast from "../Toast";
+import { useRouter } from "next/navigation";
+
+interface LoginLinkProps {
+  userId: string | undefined;
+  href: string;
+  children: ReactNode;
+}
+
+const LoginLink = ({ userId, href, children }: LoginLinkProps) => {
+  const { show } = Toast();
+  const router = useRouter();
+
+  const handleLinkClick = (e: MouseEvent<HTMLAnchorElement>) => {
+    if (!userId) {
+      e.preventDefault();
+      show("로그인이 필요한 서비스입니다.", "warn-solid");
+      router.push("/login");
+    }
+  };
+
+  return (
+    <Link href={href} onClick={handleLinkClick}>
+      {children}
+    </Link>
+  );
+};
+export default LoginLink;
--- src/app/_component/common/Toast/index.tsx
+++ src/app/_component/common/Toast/index.tsx
@@ -1,41 +1,44 @@
 "use client";
 
 import { createRoot } from "react-dom/client";
-
 import ToastManager from "./ToastManager";
 import { ToastCreate, ToastIconId } from "./type";
 import { useEffect, useState } from "react";
 
+let createToastInstance: ToastCreate | undefined;
+
 const Toast = () => {
   const portalId = "toast-portal";
-  const [createToast, setCreateToast] = useState<ToastCreate>();
+  const [hasPortal, setHasPortal] = useState(false);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
       const portalElement = document.getElementById(portalId);
-      if (portalElement === undefined) {
-        return;
-      }
-      const newPortalElement = document.createElement("div");
-      newPortalElement.id = portalId;
-      document.body.appendChild(newPortalElement);
+      if (!portalElement) {
+        const newPortalElement = document.createElement("div");
+        newPortalElement.id = portalId;
+        document.body.appendChild(newPortalElement);
 
-      createRoot(newPortalElement).render(
-        <ToastManager
-          bind={(createToast) => {
-            setCreateToast(() => createToast);
-          }}
-        />
-      );
+        createRoot(newPortalElement).render(
+          <ToastManager
+            bind={(createToast) => {
+              createToastInstance = createToast;
+              setHasPortal(true);
+            }}
+          />
+        );
+      } else {
+        setHasPortal(true);
+      }
     }
   }, []);
 
   const show = (message: string, iconId: ToastIconId, duration = 2000) => {
-    if (!createToast) throw new Error("ToastManager 초기화 오류");
-    createToast(message, iconId, duration);
+    if (!createToastInstance) throw new Error("ToastManager 초기화 오류");
+    createToastInstance(message, iconId, duration);
   };
 
-  return { show };
+  return { show, hasPortal };
 };
 
 export default Toast;
