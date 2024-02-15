c14bc6cc23ff2372cabfeeb060cee503327c09d9
JangGwon <tlsakch510@naver.com>
2024-02-15 14:12:32 +0900

    temp

--- src/app/_component/common/ProductCard/index.tsx
+++ src/app/_component/common/ProductCard/index.tsx
@@ -1,12 +1,17 @@
 import Image from "next/image";
+import { StaticImageData } from "next/image";
+
+import { cn } from "@/utils/cn";
+import { ProductStateVariants } from "./ProductState.variatns";
 import getPastTime from "@/utils/getPastTime";
 
 interface ProductCard {
-  titleImage: File;
+  titleImage: StaticImageData;
   productName: string;
   createDate: Date;
   price: number;
-  tradeState: "취소" | "완료" | "거래중";
+  tradeState: "취소됨" | "거래완료" | "거래중" | "입찰실패";
+  className?: string;
 }
 
 const ProductCard = ({
@@ -14,14 +19,29 @@ const ProductCard = ({
   productName,
   createDate,
   price,
+  tradeState,
+  className,
 }: ProductCard) => {
   return (
-    <div className="flex">
-      <Image src={URL.createObjectURL(titleImage)} alt="titleImage" />
-      <div>
-        <p>{productName}</p>
-        <p>{getPastTime(createDate)}</p>
-        <p>{price}</p>
+    <div className={`w-[16rem] flex gap-6 ${className}`}>
+      <Image
+        className="w-[85px] h-[75px] bg-slate-100"
+        src={titleImage}
+        alt="titleImage"
+      />
+      <div className="w-full">
+        <p className="w-full h-[1.4rem] text-[14px] text-ellipsis overflow-hidden">
+          {productName}
+        </p>
+        <p className="text-[8px]">낙찰가 : {price}원</p>
+        <div className="flex justify-between my-1">
+          <span className={cn(ProductStateVariants({ state: tradeState }))}>
+            {tradeState}
+          </span>
+          <p className="text-[8px] text-[#ABABAB] my-1 text-right">
+            {getPastTime(createDate)}
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,40 @@
 "use client";
 
+import tempImage from "../../public/assets/logoIcon.png";
+import ProductCard from "./_component/common/ProductCard";
+
 if (process.env.NODE_ENV === "development") {
   import("@/utils/mockServer");
 }
 
 export default function Home() {
-  return <main className=""></main>;
+  const date = new Date();
+  return (
+    <main className="">
+      <ProductCard
+        titleImage={tempImage}
+        productName="멋진 아이콘"
+        price={30000}
+        createDate={date}
+        tradeState="취소됨"
+        className="mx-auto my-4"
+      />
+      <ProductCard
+        titleImage={tempImage}
+        productName="가나다라마바사아자차캍파ㅏ하기니디"
+        price={30000}
+        createDate={date}
+        tradeState="거래완료"
+        className="mx-auto my-4"
+      />
+      <ProductCard
+        titleImage={tempImage}
+        productName="가나다라마바사아자차카ㅏ하기니디"
+        price={30000}
+        createDate={date}
+        tradeState="거래완료"
+        className="border-[0.1rem] border-black px-1 py-1 mx-auto"
+      />
+    </main>
+  );
 }
