b24c1cc7861212c24b70392dcf10c4f07217fa42
JangGwon <tlsakch510@naver.com>
2024-02-16 02:46:27 +0900

    test : 테스트 코드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,9 @@
 "use client";
-import UserCard from "./_component/common/UserCard";
-import temp from "../../public/logoIcon.png";
 
 if (process.env.NODE_ENV === "development") {
   import("@/utils/mockServer");
 }
 
 export default function Home() {
-  return (
-    <main className="">
-      <UserCard className="gap-2 border-2 border-black">
-        <UserCard.Avatar
-          src={temp.src}
-          size="large"
-          rounded={"full"}
-          className="bg-black"
-        />
-        <UserCard.ContentArea className="my-auto">
-          <p>text</p>
-          <p>text</p>
-        </UserCard.ContentArea>
-      </UserCard>
-    </main>
-  );
+  return <main className=""></main>;
 }
