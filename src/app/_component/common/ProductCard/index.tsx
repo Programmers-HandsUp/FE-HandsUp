61cceb9bf76eb93b1bbd94d45c14467904c78cb1
JangGwon <tlsakch510@naver.com>
2024-02-15 16:00:33 +0900

    feat : 돈 3자리단위마다 ,찍히게하는 유닛함수 추가 및 ProductCard에 적용

--- src/app/_component/common/ProductCard/index.tsx
+++ src/app/_component/common/ProductCard/index.tsx
@@ -3,6 +3,7 @@ import { StaticImageData } from "next/image";
 
 import { cn } from "@/utils/cn";
 import { ProductStateVariants } from "./ProductState.variatns";
+import setMoneyUnityString from "@/utils/setMoneyUnityString";
 import getPastTime from "@/utils/getPastTime";
 
 interface ProductCard {
@@ -30,8 +31,7 @@ const ProductCard = ({
   tradeState,
   className,
 }: ProductCard) => {
-  console.log(width);
-  return (
+  https: return (
     <button onClick={onClickProductCard} className={`flex gap-6 ${className}`}>
       <Image
         className="w-[85px] h-[75px] bg-slate-100"
@@ -42,7 +42,9 @@ const ProductCard = ({
         <p className="w-[9rem] h-[1.4rem] text-left text-[14px] overflow-hidden text-ellipsis whitespace-nowrap">
           {productName}
         </p>
-        <p className="text-[8px] text-left">낙찰가 : {price}원</p>
+        <p className="text-[8px] text-left">
+          낙찰가 : {setMoneyUnityString(price)}원
+        </p>
         <div className="flex justify-between my-1">
           {isShowStateTag && (
             <span className={cn(ProductStateVariants({ state: tradeState }))}>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,5 +8,57 @@ if (process.env.NODE_ENV === "development") {
 }
 
 export default function Home() {
-  return <main className=""></main>;
+  const date = new Date();
+  return (
+    <main className="">
+      <ProductCard
+        titleImage={tempImage}
+        productName="멋진 아이콘"
+        price={30000}
+        createDate={date}
+        tradeState="취소됨"
+        className="mx-auto my-4"
+      />
+      <ProductCard
+        titleImage={tempImage}
+        productName="가나다라마바사아자차카타하기니디"
+        price={30000}
+        createDate={date}
+        tradeState="거래완료"
+        className="mx-auto my-4"
+      />
+      <ProductCard
+        titleImage={tempImage}
+        productName="가나다라마바사아자차카ㅏ하기니디"
+        price={30000}
+        createDate={date}
+        tradeState="거래중"
+        className="border-[0.1rem] border-black px-1 py-1 mx-auto"
+      />
+      <ProductCard
+        titleImage={tempImage}
+        productName="가나다라마바사아자차카ㅏ하기니디"
+        price={30000}
+        createDate={date}
+        tradeState="입찰실패"
+        className="w-[360px] px-1 py-1 mx-auto"
+      />
+      <ProductCard
+        titleImage={tempImage}
+        productName="가나다라마바사아자차카ㅏ하기니디"
+        price={30000}
+        createDate={date}
+        isShowStateTag={false}
+        tradeState="거래완료"
+        className="w-[360px] border-[0.1rem] border-black px-1 py-1 mx-auto"
+      />
+      <ProductCard
+        titleImage={tempImage}
+        productName="가나다라마바사아자차카ㅏ하기니디"
+        price={30000}
+        createDate={date}
+        className="px-1 py-1 mx-auto"
+      />
+    </main>
+  );
 }
