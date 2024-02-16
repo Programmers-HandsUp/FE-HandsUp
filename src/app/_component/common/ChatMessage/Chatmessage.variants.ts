f77f063ba1583d2f29a93c72feb4c5d4fd4ca7e1
hj9118 <hwl9118@gmail.com>
2024-02-16 15:16:40 +0900

    refactor: 레이아웃 수정

--- src/app/_component/common/ChatMessage/Chatmessage.variants.ts
+++ src/app/_component/common/ChatMessage/Chatmessage.variants.ts
@@ -1,12 +1,12 @@
 import { cva } from "class-variance-authority";
 
 export const ChatMessageVariants = cva(
-  "px-4 py-2 my-1 max-w-56 whitespace-pre-line",
+  "px-4 py-2 mt-2 max-w-56 whitespace-pre-line",
   {
     variants: {
       sender: {
         me: "bg-[#96E4FF] rounded-l-3xl rounded-br-2xl hover:bg-[#96E4FF]/50 self-end",
-        you: "bg-[#D9D9D9] rounded-r-3xl rounded-bl-2xl hover:bg-[#D9D9D9]/50 self-start",
+        you: "bg-[#D9D9D9] ml-14 rounded-r-3xl rounded-bl-2xl hover:bg-[#D9D9D9]/50 self-start",
       }
     },
     defaultVariants: {
--- src/app/_component/common/ChatMessage/index.tsx
+++ src/app/_component/common/ChatMessage/index.tsx
@@ -1,19 +1,35 @@
 import { cn } from "@/utils/cn";
 import { ChatMessageVariants } from "./Chatmessage.variants";
 import { VariantProps } from "class-variance-authority";
+import Avatar from "../Avatar";
 
 interface Props extends VariantProps<typeof ChatMessageVariants> {
-  avatar?: string;
+  avatar: string;
   message: string;
   sender: "me" | "you";
+  previousSender: "me" | "you" | null;
 }
 
-const ChatMessage = ({ avatar, message, sender, ...props }: Props) => {
+const ChatMessage = ({
+  avatar,
+  message,
+  sender,
+  previousSender,
+  ...props
+}: Props) => {
   return (
-    <div className={cn(ChatMessageVariants({ sender }))} {...props}>
-      {avatar && <img src={avatar} alt="Avatar" className="mr-2" />}
-      <span>{message}</span>
-    </div>
+    <>
+      <div {...props} className='flex flex-col'>
+        <div className='absolute'>
+          {sender === "you" && previousSender !== "you" && (
+            <Avatar src={avatar} alt='Avatar' rounded='full' />
+          )}
+        </div>
+        <div className={cn(ChatMessageVariants({ sender }))}>
+          <span>{message}</span>
+        </div>
+      </div>
+    </>
   );
 };
 
