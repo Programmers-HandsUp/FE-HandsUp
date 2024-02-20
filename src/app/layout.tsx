ad235a7849f245a00962d6691f403e62a8e11d57
신준혁 <90139306+oridori2705@users.noreply.github.com>
2024-02-20 11:41:26 +0900

    fix: cookie로 다크모드 설정 (#63)
    * fix: cookie로 다크모드 설정
    
    * refactor: 쿠키get, set 고차함수 사용 및 구조분해 할당 사용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import ThemeScript from "./_component/ThemeScript";
-
+import ThemeButton from "./_component/common/ThemeButton";
+import { cookies } from "next/headers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -10,15 +10,19 @@ export const metadata: Metadata = {
   description: "Used goods traded at auction",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cookieStore = cookies();
+  const savedDarkMode = cookieStore.get("theme");
+
+  const initialDarkMode =
+    savedDarkMode === undefined ? "dark" : savedDarkMode.value;
   return (
-    <html lang="en" suppressHydrationWarning={true}>
+    <html lang="en" className={initialDarkMode}>
       <body className={inter.className}>
-        <ThemeScript />
         <main className="relative mx-auto h-[100dvh] max-w-[360px] overscroll-y-none px">
           {children}
         </main>
