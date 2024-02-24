1bb76013663b46c7bb77f0d4783934d22955add6
신준혁 <90139306+oridori2705@users.noreply.github.com>
2024-02-24 16:46:44 +0900

    feat: useModal 구현 (#76)
    * feat: useModal 구현
    
    * feat: useModal 타입에 따른 버그 수정
    
    * chore: 콘솔로그 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,3 @@
-
-"use client";
-
-import React from "react";
-
-if (process.env.NODE_ENV === "development") {
-  import("@/utils/mockServer");
-}
 export default function Home() {
-  return (
-    <main className="">
-    </main>
-  );
+  return <main className=""></main>;
 }
