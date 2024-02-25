484afc8ca13b3fe1e2296a54b31e2126cc1ff3a2
imssein <hwinnie131@gmail.com>
2024-02-26 01:30:00 +0900

    feat: datepicker 라이브러리를 이용한 경매 마감일 구현

--- src/app/products/new/_components/AuctionInfo/index.tsx
+++ src/app/products/new/_components/AuctionInfo/index.tsx
@@ -2,6 +2,9 @@ import InputLabel from "../InputLabel";
 import { RegisterProduct } from "../../page";
 import { Controller, useFormContext } from "react-hook-form";
 import InputPrice from "@/app/_component/common/InputPrice";
+import Icon from "@/app/_component/common/Icon";
+import Tooltip from "@/app/_component/common/Tooltip";
+import Datepicker from "react-tailwindcss-datepicker";
 
 function AuctionInfo() {
   const {
@@ -11,6 +14,10 @@ function AuctionInfo() {
     register,
     formState: { errors }
   } = useFormContext<RegisterProduct>();
+  let maxDate = new Date();
+  maxDate.setDate(maxDate.getDate() + 7);
+
+  const inputCount = watch("description") ? watch("description").length : 0;
 
   return (
     <div className="m-2">
@@ -27,6 +34,39 @@ function AuctionInfo() {
               price={watch("price")}
               field={field}
               handleReset={() => reset()}
+            />
+          )}
+        />
+      </InputLabel>
+      <InputLabel
+        title={
+          <div className="my-3 flex items-center gap-2">
+            <span>경매 마감일</span>
+            <Tooltip
+              eventType="hover"
+              position="right"
+              content="최소 1일 / 최대 7일까지 선택 가능해요">
+              <Icon
+                id="info"
+                fill="none"
+                stroke="black"
+              />
+            </Tooltip>
+          </div>
+        }
+        name="dateRangeTime"
+        errors={errors}>
+        <Controller
+          control={control}
+          name="dateRangeTime"
+          render={({ field }) => (
+            <Datepicker
+              minDate={new Date()}
+              maxDate={maxDate}
+              placeholder={"날짜 선택하기"}
+              useRange={false}
+              value={field.value}
+              onChange={field.onChange}
             />
           )}
         />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,9 @@ const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js"
   ],
   darkMode: "class",
   theme: {
