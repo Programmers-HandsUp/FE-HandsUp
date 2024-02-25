fa334ce079da29f81990fb7c1b01a6bd54c38bac
imssein <hwinnie131@gmail.com>
2024-02-26 01:32:20 +0900

    feat: 상세 정보 입력 TextArea form에 추가

--- src/app/products/new/_components/AuctionInfo/index.tsx
+++ src/app/products/new/_components/AuctionInfo/index.tsx
@@ -91,6 +91,16 @@ function AuctionInfo() {
           )}
         />
       </InputLabel>
+      <InputLabel
+        name="description"
+        errors={errors}>
+        <textarea
+          {...register("description")}
+          style={{ resize: "none" }}
+          className="mt-4 p-4 border border-gray-200 w-full h-48 rounded-lg"
+          placeholder={"상품 및 경매 관련해서 추가적인 정보를 입력해주세요. \n안전하고 건전한 거래환경을 만들어가요 🙌🏻"}></textarea>
+        <div className="flex justify-end">{inputCount}/1000</div>
+      </InputLabel>
     </div>
   );
 }
