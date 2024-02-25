22625109061f9f6c02bc352a10f34612f357cb7d
imssein <hwinnie131@gmail.com>
2024-02-26 03:01:49 +0900

    design: slide-down 애니메이션 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,13 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   darkMode: "class",
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
       },
       animation: {
         tiltAndScale: "tiltAndScale linear 1.2s infinite",
@@ -26,7 +21,8 @@ const config: Config = {
         growOut: "growOut ease 0.3s forwards",
         fill: "fill 1s linear forwards",
         slideIn: "slideIn ease 0.3s forwards",
-        slideOut: "slideOut ease 0.3s forwards"
+        slideOut: "slideOut ease 0.3s forwards",
+        slideDown: "slideDown 1s ease-in-out forwards"
       },
       fontFamily: {
         sans: ["Yeongdeok"]
