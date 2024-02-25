cc51334c6178e166a1f41f80f2f933bec0987d19
JangGwon <tlsakch510@naver.com>
2024-02-26 00:04:57 +0900

    test : 테스트 코드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,3 @@
-"use client";
-
-import React from "react";
-import ProgressBar from "./_component/common/ProgressBar";
-
 export default function Home() {
-  return (
-    <main className="w-[10rem] h-[4rem]">
-      <ProgressBar
-        width={300}
-        height={30}
-        round={"full"}
-        percent={50}
-      />
-    </main>
-  );
+  return <main></main>;
 }
