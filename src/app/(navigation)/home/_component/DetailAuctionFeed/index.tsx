e7dfbf6b92045e80f9e18a8a6d14670d083c8b2f
신준혁 <oridori2705@naver.com>
2024-02-28 21:56:36 +0900

    refactor: api 변경 및 슬라이드 갯수 제한, Timer 컴포넌트 추가

--- src/app/(navigation)/home/_component/DetailAuctionFeed/index.tsx
+++ src/app/(navigation)/home/_component/DetailAuctionFeed/index.tsx
@@ -1,49 +1,15 @@
 "use client";
 
 import SlideCarousel from "@/app/_component/common/SlideCarousel";
+import Timer from "@/app/_component/common/Timer";
 import { getHotAuctionRecommends } from "@/app/api/auctionRecommends";
+import { Auction } from "@/utils/mocks/api/types";
 import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 
-export interface Auction {
-  seller_id: number;
-  buyer_id: number;
-  product_id: number;
-  title: string;
-  init_price: number;
-  end_date: string;
-  si: string;
-  gu: string;
-  dong: string;
-  bookmark_count: number;
-  bidding_user_count: number;
-  trade_method: string;
-  auction_status: string;
-  trading_order: number;
-  product: Product;
-}
-
-export interface Product {
-  status: string;
-  description: string;
-  purchase_time: string;
-  product_category_id: number;
-  category: Category;
-  product_image: ProductImage;
-}
-
-export interface Category {
-  product_category_value: string;
-}
-
-export interface ProductImage {
-  image_url: string;
-  product_id: number;
-}
-
 const DetailAuctionFeed = () => {
   const { data, isPending } = useQuery<Auction[]>({
-    queryKey: ["auction", "hot"],
+    queryKey: ["auction"],
     queryFn: getHotAuctionRecommends,
     staleTime: 60 * 1000,
     gcTime: 300 * 1000
@@ -51,34 +17,49 @@ const DetailAuctionFeed = () => {
 
   if (isPending) return <div>Loading...</div>;
   return (
-    <SlideCarousel
-      childSize={200}
-      groupGap={15}
-      className="py-5"
-      height={200}
-      useNav>
-      {data?.map((auction) => (
-        <div key={auction.product_id}>
-          <div className="relative w-full h-full border rounded-lg overflow-hidden">
-            <Image
-              src={auction.product.product_image.image_url}
-              width={0}
-              height={0}
-              alt="상품 이미지"
-              style={{ width: "100%", height: "100%", objectFit: "cover" }}
-            />
-            <div className="absolute flex justify-center bottom-0 w-full h-[30px] bg-gradient-to-t from-[rgba(0,0,0,0.7)]">
-              00:12:32
+    <div>
+      <div className="text-2xl">
+        <h1>지금 핫한 경매</h1>
+      </div>
+      <SlideCarousel
+        childSize={360}
+        groupGap={15}
+        className="py-5"
+        height={360}
+        useNav>
+        {data?.slice(0, 6).map((auction) => (
+          <div
+            key={auction.product_id}
+            className="group">
+            <div className="relative w-full h-full border rounded-lg overflow-hidden">
+              <Image
+                src={auction.product.product_image.image_url}
+                width={0}
+                height={0}
+                alt="상품 이미지"
+                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+              />
+              <div className="absolute flex justify-center flex-col items-center bottom-0 w-full h-[30px] bg-gradient-to-t from-[rgba(0,0,0,0.7)] transition-all group-hover:h-[80px]">
+                <div>
+                  <Timer
+                    createdAt={new Date(auction.createdAt)}
+                    deadline={new Date(auction.end_date)}
+                  />
+                </div>
+                <div className="hidden group-hover:block">
+                  현재 입찰 가격 : {auction.init_price}
+                </div>
+              </div>
+            </div>
+            <div className="text-xs text-center">
+              <span>
+                🔥{auction.bidding_user_count}명이 치열하게 참여하고있어요!
+              </span>
             </div>
           </div>
-          <div className="text-xs text-center">
-            <span>
-              🔥{auction.bidding_user_count}명이 치열하게 참여하고있어요!
-            </span>
-          </div>
-        </div>
-      ))}
-    </SlideCarousel>
+        ))}
+      </SlideCarousel>
+    </div>
   );
 };
 
