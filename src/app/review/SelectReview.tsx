ade571301c7dcb5220ebe4757745e206c5e65291
hj9118 <hwl9118@gmail.com>
2024-02-28 19:10:49 +0900

    modify: Chip value 수정

--- src/app/review/SelectReview.tsx
+++ src/app/review/SelectReview.tsx
@@ -13,13 +13,13 @@ const SelectReview = ({ onSelected }) => {
       Items={selectedItems}
       setItems={setSelectedItems}
       multiple={true}>
-      <Chip value="option1">응답이 빨라요</Chip>
-      <Chip value="option2">약속을 잘 지켜요</Chip>
-      <Chip value="option3">친절하고 매너 있어요</Chip>
-      <Chip value="option4">직접 와서 거래해요</Chip>
-      <Chip value="option5">설명을 잘 해줘요</Chip>
-      <Chip value="option6">물품 상태 사진과 같아요</Chip>
-      <Chip value="option7">가격이 착해요</Chip>
+      <Chip value="quickResponse">응답이 빨라요</Chip>
+      <Chip value="keepPromise">약속을 잘 지켜요</Chip>
+      <Chip value="kindMannered">친절하고 매너 있어요</Chip>
+      <Chip value="directTrade">직접 와서 거래해요</Chip>
+      <Chip value="goodExplanation">설명을 잘 해줘요</Chip>
+      <Chip value="sameProduct">물품 상태 사진과 같아요</Chip>
+      <Chip value="goodPrice">가격이 착해요</Chip>
     </Chips>
   );
 };
