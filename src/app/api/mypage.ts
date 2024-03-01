ed58c93e975a1932f0b400b6d8a6bfa6f1117ce3
imssein <hwinnie131@gmail.com>
2024-03-02 00:48:45 +0900

    feat: 판매 내역 tanstack query + api fetch 구현

--- src/app/api/mypage.ts
+++ src/app/api/mypage.ts
@@ -1,5 +1,21 @@
+import { Purchase, Review, ReviewLabel, Sale, Status } from "../my/types";
+
 export const purchaseList = async (status?: Status): Promise<Purchase[]> => {
   let url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/users/buy`;
+  status && (url += `?status=${status}`);
+
+  const res = await fetch(url);
+
+  if (!res.ok) throw new Error("Fail!");
+
+  return res.json();
+};
+
+export const saleList = async (
+  userId: number,
+  status?: Status
+): Promise<Sale[]> => {
+  let url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/users/${userId}/sales`;
   status && (url += `?status=${status}`);
 
   const res = await fetch(url);
