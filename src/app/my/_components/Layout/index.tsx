fd6c66564771049547541b7c4ab3c8ae360dc4f7
imssein <hwinnie131@gmail.com>
2024-03-02 00:51:21 +0900

    design: 마이페이지에서 사용하는 레이아웃 컴포넌트 구현

---
+++ src/app/my/_components/Layout/index.tsx
@@ -0,0 +1,28 @@
+import Icon from "@/app/_component/common/Icon";
+import Link from "next/link";
+import { PropsWithChildren } from "react";
+
+interface LayoutProps {
+  title: React.ReactNode;
+  url?: "buying" | "selling" | "review";
+}
+
+function Layout({ title, url, children }: PropsWithChildren<LayoutProps>) {
+  return (
+    <div className="my-6">
+      {url ? (
+        <Link href={`/my/${url}`}>
+          <div className="flex justify-between items-center text-lg font-semibold">
+            <div>{title}</div>
+            <Icon id="arrow-right" />
+          </div>
+        </Link>
+      ) : (
+        <p className="text-lg font-semibold">{title}</p>
+      )}
+      {children}
+    </div>
+  );
+}
+
+export default Layout;
