bc9deb02e274dc242a50cfae48f48d6a14bb461e
imssein <hwinnie131@gmail.com>
2024-03-02 00:49:32 +0900

    feat: 리뷰 라벨 tanstack query + api fetch 구현

--- src/app/api/mypage.ts
+++ src/app/api/mypage.ts
@@ -24,3 +24,16 @@ export const saleList = async (
 
   return res.json();
 };
+
+export const reviewLabelList = async (
+  userId: number
+): Promise<ReviewLabel[]> => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/users/${userId}/reviews/labels`
+  );
+
+  if (!res.ok) throw new Error("Fail");
+
+  return res.json();
+};
+
