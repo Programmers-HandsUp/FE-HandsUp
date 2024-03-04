cd0641028815795348febd16f574f533bcb9ff90
imssein <hwinnie131@gmail.com>
2024-03-04 12:36:54 +0900

    refactor: slug값에 따른 컴포넌트 매핑 객체 매핑으로 수정

--- src/app/my/[slug]/page.tsx
+++ src/app/my/[slug]/page.tsx
@@ -1,18 +1,17 @@
 import AllReviews from "../_components/AllReviews";
 import TransactionList from "../_components/TransactionList";
 
-function MyPage({ params }: { params: { slug: string } }) {
+type Slug = "review" | "buying" | "selling";
+function MyPage({ params }: { params: { slug: Slug } }) {
   const { slug } = params;
 
-  if (slug === "profile-edit") {
-    return;
-  } else if (slug === "review") {
-    return <AllReviews />;
-  } else if (slug === "buying") {
-    return <TransactionList tradeMethod="구매" />;
-  } else if (slug === "selling") {
-    return <TransactionList tradeMethod="판매" />;
-  }
+  const componentMap: Record<Slug, JSX.Element> = {
+    review: <AllReviews />,
+    buying: <TransactionList tradeMethod="구매" />,
+    selling: <TransactionList tradeMethod="판매" />
+  };
+
+  return componentMap[slug] || null;
 }
 
 export default MyPage;
