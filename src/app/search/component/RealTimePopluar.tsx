f6a4e7b2eccff75c4863435ca80c6e4789a658f4
JangGwon <tlsakch510@naver.com>
2024-03-04 12:27:21 +0900

    Feta ; 인기 키워드 불러오기 관련 useQuery 구현 및 적용

--- src/app/search/component/RealTimePopluar.tsx
+++ src/app/search/component/RealTimePopluar.tsx
@@ -1,31 +1,31 @@
+import useGetPopluarKeywrodrQuery from "@/app/hooks/queries/useGetPopluarKeyword";
+
+/* @TODO : 
+    로딩텍스트 스켈레톤으로 대체 예정
+  */
+
 const RealTimePopluar = () => {
-  const popularList = [
-    "아이폰 15",
-    "프로그래머스",
-    "Nextjs13",
-    "React-query",
-    "모던 딥 자바스크립트",
-    "모던 딥 리엑트",
-    "아이스크림",
-    "BBQ 치킨",
-    "스타벅스",
-    "아파트"
-  ];
+  const { data: popularKeywords } = useGetPopluarKeywrodrQuery();
+
   return (
     <div>
       <h1 className="text-xl">실시간 인기</h1>
       <div className="grid grid-rows-5 grid-flow-col my-1">
-        {popularList.map((item, index) => (
-          <button
-            onClick={() => console.log(item)}
-            className="flex gap-3"
-            key={index}>
-            <p className="text-red-500 text-[0.8rem] py-[0.2rem]">{`${index + 1} `}</p>
-            <p className="text-[0.9rem] overflow-hidden text-ellipsis">
-              {item}
-            </p>
-          </button>
-        ))}
+        {popularKeywords ? (
+          popularKeywords.map((item, index) => (
+            <button
+              onClick={() => console.log(item)}
+              className="flex gap-3"
+              key={index}>
+              <p className="text-red-500 text-[0.8rem] py-[0.2rem]">{`${index + 1} `}</p>
+              <p className="text-[0.9rem] overflow-hidden text-ellipsis">
+                {item}
+              </p>
+            </button>
+          ))
+        ) : (
+          <p> 로딩중 ...</p>
+        )}
       </div>
     </div>
   );
