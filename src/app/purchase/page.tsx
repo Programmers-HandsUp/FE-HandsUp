da05357e1e24cb24aa346c0ae73c95bc848efb14
hj9118 <hwl9118@gmail.com>
2024-03-04 10:33:59 +0900

    feat: react-hook-form 에러 수정 및 조건 추가

--- src/app/purchase/page.tsx
+++ src/app/purchase/page.tsx
@@ -1,4 +1,6 @@
-import { useForm } from "react-hook-form";
+"use client";
+
+import { Controller, useForm } from "react-hook-form";
 import Button from "../_component/common/Button";
 import InputPrice from "../_component/common/InputPrice";
 import ProductCard from "../_component/common/ProductCard";
@@ -7,38 +9,67 @@ import AuctionBanner from "./AuctionBanner";
 import AuctionRanking from "./AuctionRanking";
 import tempImage from "../../public/tempImage.png";
 
-const page = () => {
+interface Inputs {
+  price: number;
+}
+
+const PurchasePage = () => {
   const createdAt = new Date("2024-02-16T10:59:59");
   const deadline = new Date("2024-02-17T16:59:59");
+  const MAX_PRICE = 30000;
 
-  const { register, handleSubmit, setValue, reset } = useForm();
+  const { control, handleSubmit, setValue, getValues } = useForm<Inputs>();
+
+  const onSubmit = (data: Inputs) => {
+    console.log(data);
+  };
 
   return (
-    <div style={{ margin: "0 auto" }}>
-      <Timer
-        deadline={deadline}
-        createdAt={createdAt}
-      />
+    <div style={{ margin: "20px auto" }}>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Timer
+          deadline={deadline}
+          createdAt={createdAt}
+        />
+      </div>
       <AuctionBanner />
-      <ProductCard
-        titleImage={tempImage}
-        productName="상품명"
-        createDate={new Date("2024-02-26T21:29:00")}
-        price={100000}
-      />
-      <AuctionRanking />
-      <form>
-        <InputPrice
-          {...register("price")}
-          title="제안 가격"
-          setValue={setValue}
-          reset={reset}
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <ProductCard
+          titleImage={tempImage}
+          productName="상품명"
+          createDate={new Date("2024-02-26T21:29:00")}
           price={100000}
         />
-        <Button color="primary">입찰하기</Button>
+      </div>
+      <AuctionRanking />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          control={control}
+          name="price"
+          defaultValue={0}
+          render={({ field }) => (
+            <InputPrice
+              title="입찰 시작가"
+              price={field.value}
+              field={{
+                ...field,
+                onChange: (value: number) => {
+                  field.onChange(value);
+                  setValue("price", value);
+                }
+              }}
+            />
+          )}
+        />
+
+        <Button
+          color="primary"
+          disabled={getValues("price") <= MAX_PRICE}>
+          입찰하기
+        </Button>
       </form>
     </div>
   );
 };
 
-export default page;
+export default PurchasePage;
