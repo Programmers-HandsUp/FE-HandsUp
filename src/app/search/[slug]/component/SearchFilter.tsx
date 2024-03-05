2fad91372308851a357d5770139140f86e480948
JangGwon <tlsakch510@naver.com>
2024-03-06 03:19:53 +0900

    feat : 검색 페이지 필터 모달 디자인

--- src/app/search/[slug]/component/SearchFilter.tsx
+++ src/app/search/[slug]/component/SearchFilter.tsx
@@ -2,7 +2,13 @@
 
 import { useState } from "react";
 import Icon from "@/app/_component/common/Icon";
+import Input from "@/app/_component/common/Input";
 import { Chips, Chip } from "@/app/_component/common/Chips";
+import InputForm from "@/app/_component/common/Input/InputForm";
+
+interface SearchFilterModal {
+  closeModal: () => void;
+}
 
 const CATEGORY_LIST = [
   "디지털 기기",
@@ -20,29 +26,29 @@ const CATEGORY_LIST = [
   "기타중고물품"
 ] as const;
 
-const SearchFilterModal = () => {
+const SearchFilterModal = ({ closeModal }: SearchFilterModal) => {
   const [isShowCategory, setIsShowCateogory] = useState(true);
   const [selectedCategory, setSelectedCategory] = useState<string[]>([]);
   return (
-    <>
+    <div>
       <button
-        className="flex"
+        className="flex text-2xl my-1"
         onClick={() => setIsShowCateogory((isShow) => !isShow)}>
         카테고리
         <Icon
-          className={`${isShowCategory ? "-rotate-90" : "rotate-90"} my-auto`}
+          className={`${isShowCategory ? "-rotate-90" : "rotate-90"} ml-1 my-auto`}
           id="arrow-back"
           size={16}
         />
       </button>
       {isShowCategory && (
         <Chips
-          className="grid grid-cols-2 gap-y-2"
+          className="grid grid-cols-2"
           Items={selectedCategory}
           setItems={setSelectedCategory}>
           {CATEGORY_LIST.map((value) => (
             <Chip
-              className="border-0 w-[7rem] h-[2rem] my-0 text-start"
+              className="border-0 w-[7rem] h-[1.5rem] my-0 text-start"
               key={value}
               value={value}>
               {value}
@@ -50,10 +56,11 @@ const SearchFilterModal = () => {
           ))}
         </Chips>
       )}
-      <h2 className="text-xl">거래 방식</h2>
-      <div className="flex gap-[6rem]">
+      <h2 className="text-2xl mt-4">거래 방식</h2>
+      <div className="flex gap-[6.7rem] my-3 px-2">
         <div>
           <input
+            className="mr-2"
             id="direct"
             type="radio"
             name="trade"
@@ -62,6 +69,7 @@ const SearchFilterModal = () => {
         </div>
         <div>
           <input
+            className="mr-2"
             id="delivery"
             type="radio"
             name="trade"
@@ -69,18 +77,60 @@ const SearchFilterModal = () => {
           <label>택배</label>
         </div>
       </div>
-      <div className="flex gap-[6rem]">
+      <div className="flex gap-[5rem] px-2">
         <div>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            className="mr-2"
+          />
           <label>미개봉 상품</label>
         </div>
         <div>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            className="mr-2"
+          />
           <label>진행 중인 경매만 보기</label>
         </div>
       </div>
-      <h2>금액</h2>
-    </>
+      <h2 className="mt-4 text-xl">금액</h2>
+      <div className="px-1">
+        <Input className="my-1">
+          <label className="my-auto mr-4 text-lg">이상</label>
+          <Input.InputInnerBox className="w-[14rem] h-[2.8rem]">
+            <Input.InputForm
+              className="w-[11.5rem] text-start"
+              type="number"
+            />
+            <label className="text-black my-auto text-2xl">원</label>
+          </Input.InputInnerBox>
+        </Input>
+        <Input>
+          <label className="my-auto mr-4 text-lg">이하</label>
+          <Input.InputInnerBox className="w-[14rem] h-[2.8rem]">
+            <Input.InputForm
+              className="w-[11.5rem] text-start"
+              type="number"
+            />
+            <label className="text-black my-auto text-2xl">원</label>
+          </Input.InputInnerBox>
+        </Input>
+      </div>
+      <div className="flex w-[60%] mx-auto justify-between my-6 gap-2 text-xl">
+        <button
+          className="w-[8rem] h-[2.3rem] bg-blue-300 rounded-md"
+          onClick={() => {
+            closeModal();
+          }}>
+          적용
+        </button>
+        <button
+          className="w-[8rem] h-[2.3rem] bg-blue-300 rounded-md"
+          onClick={closeModal}>
+          취소
+        </button>
+      </div>
+    </div>
   );
 };
 
--- src/app/search/[slug]/page.tsx
+++ src/app/search/[slug]/page.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { useState } from "react";
 
+import useModal from "@/app/hooks/useModal";
 import SearchBar from "../component/SearchBar";
 import ProductCard from "@/app/_component/common/ProductCard";
 import SearchFilterModal from "./component/SearchFilter";
@@ -23,16 +24,33 @@ const MockData = {
   date: "Wed Mar 06 2024 00:43:11 GMT+0900",
   likeCount: 10
 };
+const MockData2 = {
+  id: 999999,
+  title: "휴대폰",
+  price: 15000,
+  dong: "성수동",
+  date: "Wed Mar 06 2024 00:43:11 GMT+0900",
+  likeCount: 10
+};
 
 const SearchResultPage = ({ params }: SearchResultPageProps) => {
-  const resultList = [MockData, MockData];
+  const resultList = [MockData, MockData2];
   const [alignOption, setAlignOption] = useState("마감 임박 순");
+  const { Modal, open, close } = useModal({
+    modalType: "fullScreen",
+    animate: "slide"
+  });
+
   return (
     <main className="w-[90%] mx-auto">
       <SearchBar />
       <h1>진행중인 경매 상품</h1>
-      <div className="flex justify-between">
-        <button>필터</button>
+      <div className="flex justify-between my-2 mb-6">
+        <button
+          onClick={open}
+          className="px-4 border-2 border-black text-black bg-white rounded-md">
+          필터
+        </button>
         <DropDown
           setSelectedOption={(selectedOption) => setAlignOption(selectedOption)}
           selectedOption={alignOption}
@@ -40,8 +58,8 @@ const SearchResultPage = ({ params }: SearchResultPageProps) => {
           options={["마감 임박 순", "저장 순", "최신 순"]}
         />
       </div>
-      {resultList.map((resultItem, index) => (
-        <div key={index}>
+      {resultList.map((resultItem) => (
+        <div key={resultItem.id}>
           <ProductCard
             className="my-2"
             id={resultItem.id}>
@@ -51,12 +69,12 @@ const SearchResultPage = ({ params }: SearchResultPageProps) => {
             />
             <ProductCard.CardTitle width={200}>
               <p className="text-2xl">{resultItem.title}</p>
-              <div className="flex gap-4 text-[1rem]">
+              <div className="flex gap-4 text-[0.85rem]">
                 <p>{resultItem.dong}</p>
                 <p>{getPastTime(new Date(resultItem.date))}</p>
               </div>
               <div className="flex justify-between">
-                <p className="font-bold text-[1.3rem]">{resultItem.price}</p>
+                <p className="font-bold text-[1.1rem]">{resultItem.price}</p>
                 <div className="flex">
                   <Image
                     className="h-fit mr-1 mt-3"
@@ -71,6 +89,9 @@ const SearchResultPage = ({ params }: SearchResultPageProps) => {
           <hr />
         </div>
       ))}
+      <Modal className="bg-black">
+        <SearchFilterModal closeModal={close}/>
+      </Modal>
     </main>
   );
 };
