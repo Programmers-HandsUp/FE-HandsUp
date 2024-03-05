d648150b80236a40bb607778453bf4e03d439f6a
JangGwon <40955023+JuJangGwon@users.noreply.github.com>
2024-03-06 03:35:18 +0900

    feat : 로그인 페이지 구현 (#102)
    * feat : 로그인 페이지 구현
    
    * feat : msw-로그인시 쿠키에 토큰 저장 기능 구현
    
    * feat : 리액트 쿼리 적용 및 소셜로그인 아이콘 적용
    
    * feat : 로그인 성공시 메인 홈으로 라우팅
    
    * refactor : mutationFn - async/await 제거
    
    * feat : 소셜로그인 중 네이버 로그인 아이콘 변경

---
+++ src/app/login/component/SocialLoginContainer.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import googleIcon from "~/google-icon.png";
+import naverIcon from "~/whiteNaverIcon.svg";
+
+const SocialLoginContainer = () => {
+  return (
+    <div>
+      <div className="flex flex-col mt-8 w-fit mx-auto">
+        <button className="w-[9rem] h-[2.2rem] rounded-md bg-[#96E4FF] flex py-[0.3rem] justify-center gap-4">
+          <Image
+            className="w-[1.3rem] bg-white border-green-200 h-[1.3rem] my-[0.1rem]"
+            src={naverIcon}
+            alt="naverIcon"
+          />
+          네이버 로그인
+        </button>
+        <button className="w-[9rem] h-[2.2rem] rounded-md bg-[#96E4FF] my-1 flex py-[0.4rem] ">
+          <Image
+            className="w-[1.3rem] h-[1.3rem] mx-[1rem]"
+            src={googleIcon}
+            alt="googleIcon"
+          />
+          구글 로그인
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SocialLoginContainer;
