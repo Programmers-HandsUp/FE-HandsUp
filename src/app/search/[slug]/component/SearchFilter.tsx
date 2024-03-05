e5f8ab4d711c39c6b888631ff21e8391f379be70
JangGwon <tlsakch510@naver.com>
2024-03-06 01:56:45 +0900

    feat : 검색 결과 리스트 구현

--- src/app/search/[slug]/component/SearchFilter.tsx
+++ src/app/search/[slug]/component/SearchFilter.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import Icon from "@/app/_component/common/Icon";
 import { Chips, Chip } from "@/app/_component/common/Chips";
 
 const CATEGORY_LIST = [
@@ -24,14 +25,24 @@ const SearchFilterModal = () => {
   const [selectedCategory, setSelectedCategory] = useState<string[]>([]);
   return (
     <>
-      <button> 카테고리</button>
+      <button
+        className="flex"
+        onClick={() => setIsShowCateogory((isShow) => !isShow)}>
+        카테고리
+        <Icon
+          className={`${isShowCategory ? "-rotate-90" : "rotate-90"} my-auto`}
+          id="arrow-back"
+          size={16}
+        />
+      </button>
       {isShowCategory && (
         <Chips
+          className="grid grid-cols-2 gap-y-2"
           Items={selectedCategory}
-          setItems={setSelectedCategory}
-          size={90}>
+          setItems={setSelectedCategory}>
           {CATEGORY_LIST.map((value) => (
             <Chip
+              className="border-0 w-[7rem] h-[2rem] my-0 text-start"
               key={value}
               value={value}>
               {value}
@@ -39,6 +50,36 @@ const SearchFilterModal = () => {
           ))}
         </Chips>
       )}
+      <h2 className="text-xl">거래 방식</h2>
+      <div className="flex gap-[6rem]">
+        <div>
+          <input
+            id="direct"
+            type="radio"
+            name="trade"
+          />
+          <label>직거래</label>
+        </div>
+        <div>
+          <input
+            id="delivery"
+            type="radio"
+            name="trade"
+          />
+          <label>택배</label>
+        </div>
+      </div>
+      <div className="flex gap-[6rem]">
+        <div>
+          <input type="checkbox" />
+          <label>미개봉 상품</label>
+        </div>
+        <div>
+          <input type="checkbox" />
+          <label>진행 중인 경매만 보기</label>
+        </div>
+      </div>
+      <h2>금액</h2>
     </>
   );
 };
--- src/app/search/[slug]/page.tsx
+++ src/app/search/[slug]/page.tsx
@@ -1,12 +1,27 @@
 import SearchBar from "../component/SearchBar";
 import ProductCard from "@/app/_component/common/ProductCard";
 import SearchFilterModal from "./component/SearchFilter";
+import getPastTime from "@/utils/getPastTime";
+import tempLogoImage from "../../../../public/logoIcon.png";
+import likeIcon from "../../../../public/assets/likeIcon.svg";
+import Image from "next/image";
 
 interface SearchResultPageProps {
   params: string;
 }
 
+const MockData = {
+  id: 12491249,
+  title: "휴대폰",
+  price: 15000,
+  dong: "성수동",
+  date: "Wed Mar 06 2024 00:43:11 GMT+0900",
+  likeCount: 10
+};
+
 const SearchResultPage = ({ params }: SearchResultPageProps) => {
+  const resultList = [MockData, MockData];
+
   return (
     <main className="w-[90%] mx-auto">
       <SearchBar />
@@ -15,10 +30,37 @@ const SearchResultPage = ({ params }: SearchResultPageProps) => {
         <button>필터</button>
         <button>추천순</button>
       </div>
-      <ProductCard id={1}>
-        <ProductCard.CardImage titleImage="/public/logoIcon.png" />
-      </ProductCard>
-      <SearchFilterModal />
+      {resultList.map((resultItem) => (
+        <div key={resultItem.id}>
+          <ProductCard
+            className="my-2"
+            id={resultItem.id}>
+            <ProductCard.CardImage
+              className="w-[6.5rem] h-[6.5rem] mr-4"
+              titleImage={tempLogoImage.src}
+            />
+            <ProductCard.CardTitle width={200}>
+              <p className="text-2xl">{resultItem.title}</p>
+              <div className="flex gap-4 text-[1rem]">
+                <p>{resultItem.dong}</p>
+                <p>{getPastTime(new Date(resultItem.date))}</p>
+              </div>
+              <div className="flex justify-between">
+                <p className="font-bold text-[1.3rem]">{resultItem.price}</p>
+                <div className="flex">
+                  <Image
+                    className="h-fit mr-1 mt-3"
+                    src={likeIcon}
+                    alt="likeButton"
+                  />
+                  <p className="text-[1rem]">{resultItem.likeCount}</p>
+                </div>
+              </div>
+            </ProductCard.CardTitle>
+          </ProductCard>
+          <hr />
+        </div>
+      ))}
     </main>
   );
 };
