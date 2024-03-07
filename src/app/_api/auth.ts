28b216d180200a9a841ed2aa87aec88223b5ce5d
JangGwon <tlsakch510@naver.com>
2024-03-08 00:32:35 +0900

    refactor : 중복된 아이디 경고 토스트 메세지 변경

--- src/app/_api/auth.ts
+++ src/app/_api/auth.ts
@@ -1,9 +1,11 @@
+import { tokenType } from "@/utils/mocks/api/types";
+
 type LoginFormValues = {
   email: string;
   password: string;
 };
 
-const setLogin = async (authData: LoginFormValues): Promise<any> => {
+const setLogin = async (authData: LoginFormValues): Promise<tokenType> => {
   const response = await fetch("http://localhost:9090/api/login", {
     method: "POST",
     headers: {
@@ -18,7 +20,7 @@ const setLogin = async (authData: LoginFormValues): Promise<any> => {
   throw new Error(response.status.toString());
 };
 
-const signUp = async (authData: LoginFormValues): Promise<any> => {
+const signUp = async (authData: LoginFormValues): Promise<tokenType> => {
   const response = await fetch("http://localhost:9090/api/signup", {
     method: "POST",
     headers: {
@@ -33,7 +35,7 @@ const signUp = async (authData: LoginFormValues): Promise<any> => {
   throw new Error(response.status.toString());
 };
 
-const idduplicatecheck = async (id: string): Promise<any> => {
+const idduplicatecheck = async (id: string): Promise<boolean> => {
   const req = await fetch(`http://localhost:9090/api/idduplicatecheck/${id}`, {
     method: "GET"
   });
