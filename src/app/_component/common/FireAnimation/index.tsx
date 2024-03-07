766c37d964b09bafd28b319a2e6b12881b34d99d
신준혁 <oridori2705@naver.com>
2024-03-07 14:20:00 +0900

    refactor: fire 컴포넌트 커스텀 기능 추가

--- src/app/_component/common/FireAnimation/index.tsx
+++ src/app/_component/common/FireAnimation/index.tsx
@@ -1,5 +1,13 @@
-const FireAnimation = () => {
-  const parts = 10;
+import { cn } from "@/utils/cn";
+
+const FireAnimation = ({
+  size = 12,
+  className
+}: {
+  size: number;
+  className?: string;
+}) => {
+  const parts = 15;
   const particles = Array.from({ length: parts }).map((_, index) => {
     const delay = Math.random() + "s";
     const left = `calc((100% - 5em) * ${index / parts})`;
@@ -14,7 +22,13 @@ const FireAnimation = () => {
       />
     );
   });
-  return <div className="fire">{particles}</div>;
+  return (
+    <div
+      className={cn(className, "fire")}
+      style={{ fontSize: `${size}px` }}>
+      {particles}
+    </div>
+  );
 };
 
 export default FireAnimation;
