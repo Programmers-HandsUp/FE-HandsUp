866a56c436f7806c8f8e2f99ede96e284fe76713
신준혁 <oridori2705@naver.com>
2024-03-08 00:54:32 +0900

    refactor: props에서 필요한 데이터만 받도록 변경

--- src/app/_component/common/AuctionDetailInfo/DefaultCase.tsx
+++ src/app/_component/common/AuctionDetailInfo/DefaultCase.tsx
@@ -1,57 +1,48 @@
 import AuctionDetailInfo from ".";
 
-interface IAuctionDetailData {
-  auctionData: {
-    auctionId: number;
-    sellerId: number;
-    title: string;
-    productCategory: string;
-    initPrice: number;
-    currentBiddingPrice: number;
-    endDate: string;
-    productStatus: string;
-    purchaseTime: string;
-    description: string;
-    tradeMethod: string;
-    imageUrls: string[];
-    si: string;
-    gu: string;
-    dong: string;
-    bookmarkCount: number;
-    createdAt: string;
-  };
+interface DefaultAuctionDetailInfoProps {
+  title: string;
+  productCategory: string;
+  productStatus: string;
+  purchaseTime: string;
+  description: string;
+  tradeMethod: string;
+  si: string;
+  gu: string;
+  dong: string;
+  bookmarkCount: number;
 }
 
 export const DefaultAuctionDetailInfo = ({
-  auctionData
-}: IAuctionDetailData) => {
+  title,
+  productCategory,
+  productStatus,
+  purchaseTime,
+  description,
+  si,
+  gu,
+  dong,
+  tradeMethod
+}: DefaultAuctionDetailInfoProps) => {
   return (
     <AuctionDetailInfo>
-      <AuctionDetailInfo.Title>{auctionData.title}</AuctionDetailInfo.Title>
+      <AuctionDetailInfo.Title>{title}</AuctionDetailInfo.Title>
 
-      <AuctionDetailInfo.Category>
-        {auctionData.productCategory}
-      </AuctionDetailInfo.Category>
+      <AuctionDetailInfo.Category>{productCategory}</AuctionDetailInfo.Category>
 
       <AuctionDetailInfo.TagWrapper>
-        <AuctionDetailInfo.Tag>
-          {auctionData.productStatus}
-        </AuctionDetailInfo.Tag>
-        <AuctionDetailInfo.Tag>
-          {auctionData.purchaseTime}
-        </AuctionDetailInfo.Tag>
+        <AuctionDetailInfo.Tag>{productStatus}</AuctionDetailInfo.Tag>
+        <AuctionDetailInfo.Tag>{purchaseTime}</AuctionDetailInfo.Tag>
       </AuctionDetailInfo.TagWrapper>
 
       <AuctionDetailInfo.Description>
-        {auctionData.description}
+        {description}
       </AuctionDetailInfo.Description>
 
       <AuctionDetailInfo.TradeWrapper>
-        <AuctionDetailInfo.TradeRegion>
-          {auctionData.dong}
-        </AuctionDetailInfo.TradeRegion>
+        <AuctionDetailInfo.TradeRegion>{dong}</AuctionDetailInfo.TradeRegion>
         <AuctionDetailInfo.TradeMethod>
-          {auctionData.tradeMethod}
+          {tradeMethod}
         </AuctionDetailInfo.TradeMethod>
       </AuctionDetailInfo.TradeWrapper>
     </AuctionDetailInfo>
