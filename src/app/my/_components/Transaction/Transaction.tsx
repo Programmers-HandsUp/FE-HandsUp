66b43c12214ecb4264d2ebb8f3f273fc3700d01b
imssein <hwinnie131@gmail.com>
2024-03-07 17:57:55 +0900

    refactor: 거래 내역 리스트 합성 컴포넌트로 수정

---
+++ src/app/my/_components/Transaction/Transaction.tsx
@@ -0,0 +1,42 @@
+import { PropsWithChildren } from "react";
+
+import ProductCard from "@/app/_component/common/ProductCard";
+import {
+  TransactionDate,
+  TransactionHeader,
+  TransactionPrice
+} from "./TransactionItem";
+import { Purchase, Sale } from "../../types";
+
+function TransactionWrapper({
+  children,
+  transaction
+}: PropsWithChildren<{ transaction: Purchase | Sale }>) {
+  return (
+    <>
+      <ProductCard
+        key={transaction.auctionId}
+        id={transaction.auctionId}>
+        <div className="grid grid-cols-6 items-center w-full border-t py-4">
+          <ProductCard.CardImage
+            titleImage="/logoIcon.png"
+            width={60}
+            height={60}
+          />
+          <ProductCard.CardTitle className="pl-2 col-span-2 text-base overflow-hidden whitespace-nowrap text-ellipsis">
+            {transaction.auctionTitle}
+          </ProductCard.CardTitle>
+          {children}
+        </div>
+      </ProductCard>
+    </>
+  );
+}
+
+const Transaction = Object.assign(TransactionWrapper, {
+  Header: TransactionHeader,
+  Price: TransactionPrice,
+  Date: TransactionDate
+});
+
+export default Transaction;
