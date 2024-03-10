d07988e76ef560bf4d5cd44b91d4498e48115856
신준혁 <oridori2705@naver.com>
2024-03-11 01:03:28 +0900

    refactor: 판매자 정보 추가 - 임시 출력

--- src/app/products/[auctionId]/_component/DetailInfoSection.tsx
+++ src/app/products/[auctionId]/_component/DetailInfoSection.tsx
@@ -1,15 +1,17 @@
 "use client";
 
+import ArrowBackButton from "@/app/_component/common/ArrowBackButton";
 import AuctionBidsSection from "@/app/_component/common/AuctionBidsSection";
 import AuctionDetailFooterBar from "@/app/_component/common/AuctionDetailFooterBar";
 import { DefaultAuctionDetailInfo } from "@/app/_component/common/AuctionDetailInfo/DefaultCase";
 import CarouselDetailImage from "@/app/_component/common/CarouselDetailImage";
 import Comment from "@/app/_component/common/Comment";
 import Header from "@/app/_component/common/Header";
-import Icon from "@/app/_component/common/Icon";
 import LineChart from "@/app/_component/common/LineChart";
+import ReliabilityBar from "@/app/_component/common/Reliabilitybar";
 import Timer from "@/app/_component/common/Timer";
 import TopThreeRank from "@/app/_component/common/TopThreeRank";
+import UserCard from "@/app/_component/common/UserCard";
 import useBidsQuery from "@/app/hooks/queries/useBidsQuery";
 import useGetAuctionDetail from "@/app/hooks/queries/useGetAuctionDetail";
 
@@ -22,17 +24,11 @@ const DetailInfoSection = ({ auctionId }: DetailInfoSectionProps) => {
   const { top3, bids } = useBidsQuery({ auctionId });
   if (!data) return <div>데이터가 없습니다!</div>;
 
-  //TODO : 뒤로가기 버튼 컴포넌트 만들기
   return (
     <>
       <header>
         <Header
-          left={
-            <Icon
-              id="arrow-back"
-              fill="white"
-            />
-          }
+          left={<ArrowBackButton />}
           center={
             <Timer
               createdAt={data.createdAt}
@@ -50,6 +46,25 @@ const DetailInfoSection = ({ auctionId }: DetailInfoSectionProps) => {
           imageUrls={data.imageUrls}
           auctionStatus={data.auctionStatus}
         />
+        <div>
+          <UserCard className="gap-4 items-center">
+            <UserCard.Avatar
+              src="/assets/logo.webp"
+              size="medium"
+              rounded="full"
+              className="bg-slate-100"
+              alt="판매자 프로필 이미지"
+            />
+            <UserCard.ContentArea className="my-1">
+              <div className="flex flex-col">
+                <p className="text-lg">오리도리</p>
+                <p className="text-sm">성수동</p>
+              </div>
+            </UserCard.ContentArea>
+          </UserCard>
+          <ReliabilityBar score={140} />
+        </div>
+        <hr />
         <DefaultAuctionDetailInfo
           title={data.title}
           productCategory={data.productCategory}
@@ -61,6 +76,7 @@ const DetailInfoSection = ({ auctionId }: DetailInfoSectionProps) => {
           productStatus={data.productStatus}
           purchaseTime={data.purchaseTime}
         />
+        <hr />
         <LineChart bids={bids} />
         <TopThreeRank content={top3.content} />
         <Comment auctionId={auctionId} />
