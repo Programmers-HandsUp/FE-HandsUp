df0aabe44eef4067734778ce2e62a0e487774b45
신준혁 <oridori2705@naver.com>
2024-03-10 15:17:36 +0900

    refactor: Header 컴포넌트 수정

--- src/app/_component/common/Header/index.tsx
+++ src/app/_component/common/Header/index.tsx
@@ -4,24 +4,16 @@ interface HeaderProps {
   left?: ReactNode;
   center?: ReactNode;
   right?: ReactNode;
-  leftFlex?: boolean;
-  rightFlex?: boolean;
-  centerFlex?: boolean;
 }
 
-const Header = ({
-  left,
-  center,
-  right,
-  leftFlex,
-  centerFlex,
-  rightFlex,
-}: HeaderProps) => {
+const Header = ({ left, center, right }: HeaderProps) => {
   return (
     <div className="flex justify-between items-center p-3 gap-4">
-      {left && <div className={leftFlex ? "flex-1" : ""}>{left}</div>}
-      {center && <div className={centerFlex ? "flex-1" : ""}>{center}</div>}
-      {right && <div className={rightFlex ? "flex-1" : ""}>{right}</div>}
+      <div style={{ flex: 1 }}>{left || <div></div>}</div>
+      <div style={{ flex: 1, textAlign: "center" }}>
+        {center || <div></div>}
+      </div>
+      <div style={{ flex: 1, textAlign: "right" }}>{right || <div></div>}</div>
     </div>
   );
 };
