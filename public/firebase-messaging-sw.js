2b9cb5eb5cd013c1c95e6cd47e971445bdbf68c5
imssein <hwinnie131@gmail.com>
2024-03-14 00:15:12 +0900

    modify: 불필요한 코드 삭제

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-undef */
-
 importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
 importScripts(
   "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
@@ -11,20 +9,5 @@ const firebaseConfig = {
   messagingSenderId: "414381577440",
   appId: "1:414381577440:web:f8aa7e36f17b323b8eac85"
 };
-// eslint-disable-next-line no-undef
-firebase.initializeApp(firebaseConfig);
-// eslint-disable-next-line no-undef
-const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
-  console.log(
-    "[firebase-messaging-sw.js] Received background message ",
-    payload
-  );
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: "/assets/icons/icon-512x512.png"
-  };
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
+firebase.initializeApp(firebaseConfig);
