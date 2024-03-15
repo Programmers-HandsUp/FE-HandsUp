2adf295ab45860f4b5b70398b15379ca3afd3940
세인 <93479475+imssein@users.noreply.github.com>
2024-03-15 20:32:52 +0900

    feat: 사용자 알림 허용 컴포넌트 구현 (#154)
    * modify: 불필요한 코드 삭제
    
    * refactor: 파일 위치 수정
    
    * refactor: 파일 위치 수정
    
    * feat: 알림 허용 컴포넌트 구현
    
    * feat: 알림 비허용 컴포넌트 구현
    
    * design: 알림 off 아이콘 추가
    
    * modify: toast 메세지 수정
    
    * modify: 불필요한 fragment 삭제

---
+++ src/app/_component/notification/_hooks/useCheckPermission.ts
@@ -0,0 +1,34 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import Toast from "../../common/Toast";
+import useFcmToken from "./useFcmToken";
+
+const useNotificationPermission = () => {
+  const [permissionStatus, setPermissionStatus] = useState("");
+  const { getFcmToken } = useFcmToken();
+  const { show } = Toast();
+
+  useEffect(() => {
+    setPermissionStatus(
+      typeof Notification === "undefined" ? "granted" : Notification.permission
+    );
+  }, []);
+
+  const requestPermission = () => {
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        getFcmToken();
+        show("PUSH 알림을 허용했어요.", "check-solid");
+      } else if (permission === "denied") {
+        show("PUSH 알림을 취소했어요.", "warn-solid");
+      }
+      setPermissionStatus(permission);
+    });
+  };
+
+  return { permissionStatus, requestPermission };
+};
+
+export default useNotificationPermission;
