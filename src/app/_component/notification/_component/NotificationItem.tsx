3c9ede0e5b9b7a7a8ab95ee358404404a069c6e8
imssein <hwinnie131@gmail.com>
2024-03-17 00:22:04 +0900

    refactor: 알림 목록 컴포넌트 구조 개선 및 분기 처리 간소화

--- src/app/_component/notification/_component/NotificationItem.tsx
+++ src/app/_component/notification/_component/NotificationItem.tsx
@@ -1,32 +1,52 @@
-import { Notification } from "@/utils/types/notification";
+import React, { FunctionComponent } from "react";
+
+import { ImageUrl } from "@/utils/types/image";
+import { Notification, NotificationType } from "@/utils/types/notification";
 
 import ProductCard from "../../common/ProductCard";
 import AuctionNotification from "./AuctionNotification";
 import PersonalNotification from "./PersonalNotification";
 
+interface Props {
+  type: NotificationType;
+  profileImg: ImageUrl;
+}
+
+const PersonalComponent = ({ type, profileImg }: Props) => (
+  <PersonalNotification
+    type={type}
+    img={profileImg}
+  />
+);
+
+const AuctionComponent = ({ type }: { type: NotificationType }) => (
+  <AuctionNotification type={type} />
+);
+
+const NotificationComponent: Record<
+  NotificationType,
+  FunctionComponent<Props>
+> = {
+  comment: PersonalComponent,
+  chat: PersonalComponent,
+  bookmark: PersonalComponent,
+  purchase_winning: AuctionComponent,
+  canceld_purchase_winning: AuctionComponent
+};
+
 function NotificationItem({ notification }: { notification: Notification }) {
   const { notification_Id, type, content, auctionImg, profileImg } =
     notification;
-
-  const renderNotificationContent = () => {
-    const isAuctionNotification =
-      type === "purchase_winning" || type === "canceld_purchase_winning";
-
-    return isAuctionNotification ? (
-      <AuctionNotification type={type} />
-    ) : (
-      <PersonalNotification
-        type={type}
-        img={profileImg}
-      />
-    );
-  };
+  const NotificationContent = NotificationComponent[type];
 
   return (
     <ProductCard
       id={notification_Id}
       className="flex justify-between items-center py-2 gap-2">
-      {renderNotificationContent()}
+      <NotificationContent
+        type={type}
+        profileImg={profileImg}
+      />
       <p className="flex-1 text-left ml-4">{content}</p>
       <ProductCard.CardImage
         titleImage={auctionImg}
