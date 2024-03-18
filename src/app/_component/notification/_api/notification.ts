050c8f02f4b834ff765b630444b448347b0a50b9
imssein <hwinnie131@gmail.com>
2024-03-18 22:56:38 +0900

    feat: 미확인 알림 수 조회 API 연동

--- src/app/_component/notification/_api/notification.ts
+++ src/app/_component/notification/_api/notification.ts
@@ -34,3 +34,16 @@ export const notificationList = async (): Promise<Notifications> => {
 
   return res.json();
 };
+
+export const notificationBadge = async (): Promise<number> => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/notifications/count`
+  );
+
+  if (!res.ok) {
+    const errData = await res.json();
+    throw new Error(errData.message);
+  }
+
+  return res.json();
+};
