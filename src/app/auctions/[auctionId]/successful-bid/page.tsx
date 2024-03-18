a37e56185f99de3e3e05bf31e40d2ee4135f1a20
신준혁 <oridori2705@naver.com>
2024-03-18 23:23:48 +0900

    feat: 낙찰 완료페이지 구현

--- src/app/auctions/[auctionId]/successful-bid/page.tsx
+++ src/app/auctions/[auctionId]/successful-bid/page.tsx
@@ -1,20 +1,45 @@
-import { getAuctionDetail } from "@/app/_api/getAuctionDetail";
-import { useQuery } from "@tanstack/react-query";
+"use client";
+
+import Link from "next/link";
+
+import DefaultComponent from "./_component/DefaultComponent";
+import useGetSuccessfulData from "./_hooks/useGetSuccessfulData";
 
 interface SuccessFulBidPageProps {
-  params: { auctionId: number };
+  params: { auctionId: string };
 }
 
 const SuccessfulBidPage = ({ params }: SuccessFulBidPageProps) => {
   const { auctionId } = params;
-  const { data, isFetching } = useQuery({
-    queryKey: ["product", auctionId],
-    queryFn: () => getAuctionDetail({ auctionId })
+
+  const { auction, bids } = useGetSuccessfulData({
+    auctionId: Number(auctionId)
   });
 
-  if (isFetching) return <div>로딩 중</div>;
+  const loginNickname = "오리";
 
-  return <div></div>;
+  const currentBidding = bids.content.find((x) => x.tradingStatus === "진행중");
+
+  return (
+    <div className="flex flex-col gap-16">
+      <DefaultComponent
+        biddingCount={bids.content.length}
+        bookmarkCount={auction.bookmarkCount}
+        buyerNickName={loginNickname}
+        currentBiddingPrice={currentBidding?.biddingPrice}
+        initialBiddingPrice={auction.initPrice}
+        sellerNickName={auction.sellerInfo.nickname}
+        imgUrl={auction.imageUrls[0]}
+      />
+      <div className="relative flex justify-center">
+        <Link
+          href={`/auctions/${auctionId}/bidderList?sellerId=${auction.sellerInfo.userId}`}
+          className="bg-[#96E4FF] rounded-lg p-2 hover:bg-[#55d4ff] w-full text-center text-black">
+          입찰자 목록 페이지로 가기
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default SuccessfulBidPage;
