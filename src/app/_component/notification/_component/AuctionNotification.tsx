83a9c79759f9ba65f993bb1ab96ba44030540eb2
imssein <hwinnie131@gmail.com>
2024-03-18 22:57:18 +0900

    fix: 타입 수정에 따른 icon명 수정

--- src/app/_component/notification/_component/AuctionNotification.tsx
+++ src/app/_component/notification/_component/AuctionNotification.tsx
@@ -1,13 +1,11 @@
-import React from "react";
+import { Type } from "@/utils/types/notification";
 
-import { NotificationType } from "@/utils/types/notification";
+import { IconName } from "../../common/Icon/type";
 
-import Icon from "../../common/Icon";
-
-function AuctionNotification({ type }: { type: NotificationType }) {
+function AuctionNotification({ type }: { type: Type }) {
   return (
     <p className="w-12 h-12 rounded-full bg-[#96E4FF] flex justify-center items-center">
-      <Icon id={type} />
+      id={type.toLowerCase() as IconName}
     </p>
   );
 }
