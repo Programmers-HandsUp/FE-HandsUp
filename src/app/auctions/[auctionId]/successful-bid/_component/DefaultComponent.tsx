48e423c31cf41553ecea194f11d9680bb4679c00
현세인 <hwinnie131@gmail.com>
2024-03-19 17:41:40 +0900

    feat: 404 페이지 구현  (#184)
    * setting: dotlottie 추가
    
    * design: 404 애니메이션 추가
    
    * feat: not-found 페이지 구현
    
    * design: party lottie 파일 타입 수정
    
    * feat: 홈 이동 링크 추가

--- src/app/auctions/[auctionId]/successful-bid/_component/DefaultComponent.tsx
+++ src/app/auctions/[auctionId]/successful-bid/_component/DefaultComponent.tsx
@@ -1,9 +1,7 @@
 "use client";
 
+import { DotLottiePlayer } from "@dotlottie/react-player";
 import Image from "next/image";
-import Lottie from "react-lottie-player";
-
-import party from "@/utils/LottieFiles/partypopup.json";
 
 interface DefaultComponentProps {
   sellerNickName: string;
@@ -82,10 +80,10 @@ const DefaultComponent = ({
           </div>
         </div>
         <div className="absolute top-0">
-          <Lottie
+          <DotLottiePlayer
+            src="/assets/animations/Party.lottie"
+            autoplay
             loop
-            animationData={party}
-            play
             style={{ width: "100%", height: "70%" }}
           />
         </div>
