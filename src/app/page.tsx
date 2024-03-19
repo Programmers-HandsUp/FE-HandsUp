1c445026ad820d42fbe329969bf0d1eb4f55af17
신준혁 <oridori2705@naver.com>
2024-03-19 20:01:22 +0900

    chore: 불필요한 page내 코드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,3 @@
-"use client";
-
-import React from "react";
-
-import ReliabilityBar from "./_component/common/Reliabilitybar";
-import Toast from "./_component/common/Toast";
-
 export default function Home() {
-  const toast = Toast();
-  return (
-    <main className="">
-      <ReliabilityBar score={140} />
-    </main>
-  );
+  return <main className=""></main>;
 }
