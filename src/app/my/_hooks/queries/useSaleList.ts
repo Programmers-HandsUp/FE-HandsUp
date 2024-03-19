c5207c6ab93db7a4a08661e91a784bfe81fd09a1
imssein <hwinnie131@gmail.com>
2024-03-19 23:12:18 +0900

    refactor: 공통 부분 calculateCounts 함수 적용

--- src/app/my/_hooks/queries/useSaleList.ts
+++ src/app/my/_hooks/queries/useSaleList.ts
@@ -1,25 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 
-import { Status } from "@/utils/types/user/mypage";
-
 import { saleList } from "../../_api/mypage";
+import { calculateCounts } from "./useUserInfo";
 
 const useSaleList = (userId: number) => {
   return useQuery({
     queryKey: ["saleList", userId],
     queryFn: () => saleList(userId),
-    select: (data) => {
-      const initCounts: Record<Status, number> = {
-        bidding: 0,
-        pending: 0,
-        finished: 0
-      };
-
-      return data.reduce((acc, item) => {
-        acc[item.auctionStatus]++;
-        return acc;
-      }, initCounts);
-    }
+    select: calculateCounts
   });
 };
 
