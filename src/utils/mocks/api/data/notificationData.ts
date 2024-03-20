efad764105f1777e4e9adca1084eb542bf8c601f
imssein <hwinnie131@gmail.com>
2024-03-20 23:21:36 +0900

    fix: 타입 수정에 따른 mock data 수정

--- src/utils/mocks/api/data/notificationData.ts
+++ src/utils/mocks/api/data/notificationData.ts
@@ -1,75 +1,85 @@
-import { Notification } from "../../../types/notification/index";
+import { Notification } from "@/utils/types/notification";
 
 export const notification: Notification[] = [
   {
-    notification_Id: 1,
-    profileImg:
+    notificationId: 1,
+    senderProfileImageUrl:
       "https://s3.ap-northeast-2.amazonaws.com/handsup-bucket/images/f2ed3069-3c65-4cb5-97dc-2f24a64ca103.jpg",
     content: "오리님이 회원님의 경매 물품을 관심있어 해요.",
     auctionId: 1,
-    auctionImg:
+    auctionImageUrl:
       "https://s3.ap-northeast-2.amazonaws.com/handsup-bucket/images/f12a1119-fafc-4107-924f-43c9d0fef67c.png",
-    type: "bookmark"
+    notificationType: "BOOKMARK"
   },
   {
-    notification_Id: 2,
-    profileImg:
+    notificationId: 2,
+    senderProfileImageUrl:
       "https://s3.ap-northeast-2.amazonaws.com/handsup-bucket/images/f2ed3069-3c65-4cb5-97dc-2f24a64ca103.jpg",
     content: "오리님이 회원님의 경매 물품에서 애기하고 있어요.",
     auctionId: 1,
-    auctionImg:
+    auctionImageUrl:
       "https://s3.ap-northeast-2.amazonaws.com/handsup-bucket/images/f12a1119-fafc-4107-924f-43c9d0fef67c.png",
-    type: "comment"
+    notificationType: "COMMENT"
   },
   {
-    notification_Id: 3,
-    profileImg:
+    notificationId: 3,
+    senderProfileImageUrl:
       "https://s3.ap-northeast-2.amazonaws.com/handsup-bucket/images/f2ed3069-3c65-4cb5-97dc-2f24a64ca103.jpg",
     content: "오리님이 회원님과의 채팅방에서 속삭이고 있어요",
     auctionId: 1,
-    auctionImg:
+    auctionImageUrl:
       "https://s3.ap-northeast-2.amazonaws.com/handsup-bucket/images/f12a1119-fafc-4107-924f-43c9d0fef67c.png",
-    type: "chat"
+    notificationType: "CHAT"
   },
   {
-    notification_Id: 4,
-    profileImg:
+    notificationId: 4,
+    senderProfileImageUrl:
       "https://s3.ap-northeast-2.amazonaws.com/handsup-bucket/images/f2ed3069-3c65-4cb5-97dc-2f24a64ca103.jpg",
     content: "입찰하신 물품이 낙찰되었습니다.",
     auctionId: 1,
-    auctionImg:
+    auctionImageUrl:
       "https://s3.ap-northeast-2.amazonaws.com/handsup-bucket/images/f12a1119-fafc-4107-924f-43c9d0fef67c.png",
-    type: "purchase_winning"
+    notificationType: "PURCHASE_WINNING"
   },
   {
-    notification_Id: 5,
-    profileImg:
+    notificationId: 5,
+    senderProfileImageUrl:
       "https://s3.ap-northeast-2.amazonaws.com/handsup-bucket/images/f2ed3069-3c65-4cb5-97dc-2f24a64ca103.jpg",
     content: "경매 낙찰이 취소되었습니다.",
     auctionId: 1,
-    auctionImg:
+    auctionImageUrl:
       "https://s3.ap-northeast-2.amazonaws.com/handsup-bucket/images/f12a1119-fafc-4107-924f-43c9d0fef67c.png",
-    type: "canceld_purchase_winning"
+    notificationType: "CANCELD_PURCHASE_WINNING"
   },
   {
-    notification_Id: 6,
-    profileImg:
+    notificationId: 6,
+    senderProfileImageUrl:
       "https://s3.ap-northeast-2.amazonaws.com/handsup-bucket/images/f2ed3069-3c65-4cb5-97dc-2f24a64ca103.jpg",
     content: "오리님이 회원님의 경매 물품을 관심있어 해요.",
     auctionId: 1,
-    auctionImg:
+    auctionImageUrl:
       "https://s3.ap-northeast-2.amazonaws.com/handsup-bucket/images/f12a1119-fafc-4107-924f-43c9d0fef67c.png",
-    type: "bookmark"
+    notificationType: "BOOKMARK"
   },
   {
-    notification_Id: 7,
-    profileImg:
+    notificationId: 7,
+    senderProfileImageUrl:
       "https://s3.ap-northeast-2.amazonaws.com/handsup-bucket/images/f2ed3069-3c65-4cb5-97dc-2f24a64ca103.jpg",
     content: "오리님이 회원님의 경매 물품을 관심있어 해요.",
     auctionId: 1,
-    auctionImg:
+    auctionImageUrl:
       "https://s3.ap-northeast-2.amazonaws.com/handsup-bucket/images/f12a1119-fafc-4107-924f-43c9d0fef67c.png",
-    type: "bookmark"
+    notificationType: "BOOKMARK"
+  },
+  {
+    notificationId: 8,
+    senderProfileImageUrl:
+      "https://s3.ap-northeast-2.amazonaws.com/handsup-bucket/images/f2ed3069-3c65-4cb5-97dc-2f24a64ca103.jpg",
+    content: "오리님과 거래 성공",
+    auctionId: 1,
+    auctionImageUrl:
+      "https://s3.ap-northeast-2.amazonaws.com/handsup-bucket/images/f12a1119-fafc-4107-924f-43c9d0fef67c.png",
+    notificationType: "COMPLETED_PURCHASE_TRADING"
   }
 ];
 
