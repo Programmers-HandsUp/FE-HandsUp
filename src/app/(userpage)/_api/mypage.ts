c7c7988a7da227b3e1ccb9bdce054251c64cec7d
imssein <hwinnie131@gmail.com>
2024-03-20 17:42:25 +0900

    feat: 유저 프로필 조회 API 추가

--- src/app/(userpage)/_api/mypage.ts
+++ src/app/(userpage)/_api/mypage.ts
@@ -6,6 +6,7 @@ import {
   Sale,
   Status
 } from "@/utils/types/user/mypage";
+import { UserProfile } from "@/utils/types/user/users";
 
 const isTokenValid = authCheck();
 
@@ -70,6 +71,23 @@ export const reviewList = async (userId: number): Promise<Review[]> => {
   );
 
   if (!res.ok) throw new Error("Fail");
+
+  return res.json();
+};
+
+export const userProfile = async (userId: number): Promise<UserProfile> => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/users/${userId}/profiles`,
+    {
+      headers: {
+        Authorization: `Bearer ${isTokenValid}`
+      }
+    }
+  );
+
+  if (!res.ok) {
+    console.log(res);
+  }
 
   return res.json();
 };
