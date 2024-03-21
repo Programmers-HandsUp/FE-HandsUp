0a88152a04ae71d1be4668075df15d628f6295e5
imssein <hwinnie131@gmail.com>
2024-03-22 01:01:11 +0900

    fix: 상품 등록 api token 추가

--- src/app/auctions/new/_api/registerAuction.ts
+++ src/app/auctions/new/_api/registerAuction.ts
@@ -1,12 +1,16 @@
+import { authCheck } from "@/utils/function/authCheck";
 import { Auction, AuctionData } from "@/utils/types/auction/registerAuction";
 
 export const registerAction = async (
   auctionData: AuctionData
 ): Promise<Auction> => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/auctions`, {
+  const isValidToken = authCheck();
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/auctions`, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${isValidToken}`
     },
     body: JSON.stringify(auctionData)
   });
