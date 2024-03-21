9783599541c0ca3ac6cab9575a3da4caf91f614a
신준혁 <oridori2705@naver.com>
2024-03-22 01:51:27 +0900

    feat: 미들웨어 추가

---
+++ src/middleware.ts
@@ -0,0 +1,31 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+import { getLoginUserInfo } from "./app/_api/user";
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+  const auth = await getLoginUserInfo();
+  if (!auth) {
+    return NextResponse.redirect("http://localhost:3000/signin");
+  }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    "/notification",
+    "/my",
+    "/auctions/:auctionId*/purchase",
+    "/auctions/:auctionId*/bidderList",
+    "/auctions/:auctionId*/review",
+    "/auctions/:auctionId*/successful-bid",
+    "/auctions/:auctionId*/new",
+    "/chatrooms",
+    "/chatrooms/:chatRoomId*",
+    "/report",
+    "/user"
+  ]
+};
+
+// 알림, 채팅, 북마크목록, 마이페이지,입찰, 입찰 목록, 낙찰완료, 거래완료, report, user
