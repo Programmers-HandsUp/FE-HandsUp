5e0a51b2ea74026a44580c85c29c6e4ddf7dadc8
신준혁 <90139306+oridori2705@users.noreply.github.com>
2024-03-22 03:44:08 +0900

    feat: 미들웨어 추가 (#231)

--- src/app/_api/user.ts
+++ src/app/_api/user.ts
@@ -1,18 +1,19 @@
 import { fetchWithTokenRenewal } from "@/utils/function/fetchWithTokenRenewal";
 import { CheckLoginUserResponse } from "@/utils/types/user/users";
 
-export const getLoginUserInfo = async (): Promise<CheckLoginUserResponse> => {
-  try {
-    const res = await fetchWithTokenRenewal(
-      `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/users`
-    );
+export const getLoginUserInfo =
+  async (): Promise<CheckLoginUserResponse | null> => {
+    try {
+      const res = await fetchWithTokenRenewal(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/users`
+      );
 
-    if (!res.ok) {
-      throw new Error(`${res.status}`);
+      if (!res.ok) {
+        throw new Error(`${res.status}`);
+      }
+      return res.json();
+    } catch (error: any) {
+      console.error(error.message);
     }
-    return res.json();
-  } catch (error: any) {
-    console.error(error.message);
-  }
-  throw new Error("토큰 오류");
-};
+    return null;
+  };
