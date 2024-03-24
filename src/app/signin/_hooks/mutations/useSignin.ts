0379a422b43fcc71e85c0c379f6b675d7e9fa585
신준혁 <oridori2705@naver.com>
2024-03-24 14:31:54 +0900

    fix: router.push를 redirect로 변경

--- src/app/signin/_hooks/mutations/useSignin.ts
+++ src/app/signin/_hooks/mutations/useSignin.ts
@@ -1,21 +1,21 @@
 import { useMutation } from "@tanstack/react-query";
-import { useRouter } from "next/navigation";
 
 import Toast from "@/app/_component/common/Toast";
 import { setCookie } from "@/utils/function/cookie";
 import { LoginRequest } from "@/utils/types/authorization/login";
 
 import { signIn } from "../../_api/login";
+import { navigate } from "./redirectActionsHome";
 
 export function useSignIn() {
   const { show } = Toast();
-  const router = useRouter();
+
   return useMutation({
     mutationFn: (authForm: LoginRequest) => signIn(authForm),
     onSuccess: (data) => {
       setCookie({ name: "accessToken", value: data.accessToken });
       show("로그인 성공했습니다", "check-solid", 2000);
-      router.push("/home");
+      navigate();
     },
     onError: () => {
       show("로그인 실패했습니다", "warn-solid", 2000);
