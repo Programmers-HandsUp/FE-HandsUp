2a04dc9de0c86e4bd9e220cff779188c415fa7a2
imssein <hwinnie131@gmail.com>
2024-03-24 18:16:08 +0900

    feat: 더미 데이터에서 리뷰 데이터로 수정

--- src/app/auctions/[auctionId]/review/ReviewReceipt/index.tsx
+++ src/app/auctions/[auctionId]/review/ReviewReceipt/index.tsx
@@ -5,22 +5,13 @@ import Image from "next/image";
 import Link from "next/link";
 import { useEffect } from "react";
 import { useRef } from "react";
-
+import { ReviewComplete } from "@/utils/types/auction/auctionReivewEnroll";
 import Icon from "@/app/_component/common/Icon";
+import dateFormat from "./_utils/formatDate";
 
-function ReviewComplete() {
+function ReviewReceipt({ review }: { review: ReviewComplete }) {
   const reviewRef = useRef<HTMLDivElement>(null);
-
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-
-  const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}`;
-
-  const tradeMethod = "직거래";
+  const isDirect = review && review.tradeMethod === "DIRECT";
 
   const handleDownload = async () => {
     if (!reviewRef.current) return;
@@ -30,7 +21,7 @@ function ReviewComplete() {
       const canvas = await html2canvas(div, { scale: 1 });
       canvas.toBlob((blob) => {
         if (blob !== null) {
-          saveAs(blob, "result.jpeg");
+          saveAs(blob, `${review.writerNickname}거래 영수증`);
         }
       });
     } catch (error) {
@@ -63,29 +54,31 @@ function ReviewComplete() {
               alt="app logo"
             />
           </div>
-          <p className="text-sm mb-1 px-2">{formattedDate}</p>
+          <p className="text-sm mb-1 px-2">
+            {dateFormat(review.tradingCreatedAt)}
+          </p>
           <div className="flex justify-between items-center border-dashed border-y border-gray-800 px-2 pt-4 pb-3 mb-3">
             <p className="">경매 No.</p>
-            <p className="text-xl">H-001</p>
+            <p className="text-xl">H-{review.auctionId}</p>
           </div>
 
           <div className="grid grid-cols-2 px-2 text-sm mb-4">
             <div className="grid grid-rows-4 gap-1">
+              <p>구매자</p>
               <p>판매 물품</p>
-              <p>입찰 가격</p>
               <p>낙찰 가격</p>
               <p>거래 방식</p>
             </div>
             <div className="grid grid-rows-4 gap-1 text-right">
-              <p>에어팟 새상품</p>
-              <p>100,000원</p>
-              <p>120,000원</p>
+              <p>{review.writerNickname}</p>
+              <p>{review.auctionTitle}</p>
+              <p>{review.winningPrice}</p>
               <div className="flex flex-row justify-end gap-1">
                 <label className="flex items-center gap-1">
                   <input
                     type="checkbox"
                     className="mr-1"
-                    checked={tradeMethod === "직거래"}
+                    checked={isDirect}
                     disabled
                   />
                   직거래
@@ -93,18 +86,23 @@ function ReviewComplete() {
                 <label className="flex items-center gap-1">
                   <input
                     type="checkbox"
-                    checked={tradeMethod !== "직거래"}
+                    checked={!isDirect}
                     disabled
                   />
                   택배
                 </label>
               </div>
-              {tradeMethod === "직거래" && <p>한남동</p>}
+              {isDirect && <p>{review.tradingLocation.dong}</p>}
             </div>
           </div>
 
           <div className="flex flex-col items-center gap-2 mt-auto mb-3 pt-2 border-t-2 text-sm">
             <p>HandsUp에서 좋은 거래하세요 :)</p>
+            <Link
+              href={`/auctions/${review.auctionId}`}
+              className="underline">
+              상품 다시 보러가기
+            </Link>
             <Image
               src="/assets/images/barcode.webp"
               width={130}
@@ -115,7 +113,7 @@ function ReviewComplete() {
         </div>
       </div>
 
-      <div className="flex absolute bottom-10 gap-11">
+      <div className="flex absolute bottom-10 gap-11 bg:text-white">
         <Link
           href="/"
           className="flex flex-col items-center gap-3">
@@ -139,4 +137,4 @@ function ReviewComplete() {
   );
 }
 
-export default ReviewComplete;
+export default ReviewReceipt;
