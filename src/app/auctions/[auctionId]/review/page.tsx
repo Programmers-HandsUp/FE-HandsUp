96ece82a35476e3135913943d7ac2d453cde5c70
imssein <hwinnie131@gmail.com>
2024-03-24 18:18:34 +0900

    feat: 리뷰 작성  완료 후 모달 띄우기

--- src/app/auctions/[auctionId]/review/page.tsx
+++ src/app/auctions/[auctionId]/review/page.tsx
@@ -10,12 +10,20 @@ import Icon from "@/app/_component/common/Icon";
 import ProductCard from "@/app/_component/common/ProductCard";
 import useSession from "@/app/_hooks/queries/useSession";
 import formatReadableDate from "@/utils/function/formatReadableDate";
-import { AuctionReviewEnrollRequest } from "@/utils/types/auction/auctionReivewEnroll";
+import {
+  AuctionReviewEnrollRequest,
+  ReviewComplete
+} from "@/utils/types/auction/auctionReivewEnroll";
 
 import SelectRange from "./_component/SelectRange";
 import SelectReview from "./_component/SelectReview";
 import { useGetAuctionInfo } from "./_hooks/useGetAuctionInfo";
 import { usePostReview } from "./_hooks/useProduct";
+import useModalState from "@/app/_hooks/useModalState";
+import Modal from "@/app/_component/common/Modal";
+
+import { useEffect, useState } from "react";
+import ReviewReceipt from "./ReviewReceipt";
 
 interface ReviewPageProps {
   params: { auctionId: string };
@@ -42,11 +50,25 @@ const ReviewForm = ({ params }: ReviewPageProps) => {
   } = useForm<AuctionReviewEnrollRequest>({
     resolver: zodResolver(schema)
   });
+  const { open, isOpen, close } = useModalState();
+  const [review, setReview] = useState<ReviewComplete>();
 
   const onSubmit = (data: AuctionReviewEnrollRequest) => {
-    console.log(data);
-    reviewPostMutation.mutate({ auctionId: Number(auctionId), data });
+    reviewPostMutation.mutate(
+      { auctionId: Number(auctionId), data },
+      {
+        onSuccess(data) {
+          if (data !== null) {
+            setReview(data);
+          }
+        }
+      }
+    );
   };
+
+  useEffect(() => {
+    review && open();
+  }, [review, open]);
 
   const handleRangeSelected = (value: string) => {
     setValue("evaluationScore", Number(value));
@@ -67,74 +89,86 @@ const ReviewForm = ({ params }: ReviewPageProps) => {
   const currentUser = userInfo.nickname;
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="p-[10%] flex flex-col space-y-5">
-      <Icon id="arrow-back" />
-      <div style={{ textAlign: "center" }}>
-        <p>
-          {currentUser}님, <br />
-          {auction.sellerInfo.nickname}님과의 거래는 어떠셨나요?
-        </p>
-      </div>
-      <ProductCard id={1}>
-        <div className="flex w-full border-t py-4">
-          <ProductCard.CardImage
-            titleImage={auction.imageUrls[0]}
-            width={100}
-            height={100}
-            className="group-hover:[&_img]:scale-100"
-          />
-          <div className="flex grow justify-between flex-col pl-2">
-            <div>
-              <ProductCard.CardTitle className="text-lg w-[180px] overflow-hidden whitespace-nowrap text-ellipsis">
-                {auction.title}
-              </ProductCard.CardTitle>
+    <>
+      <form
+        className="p-[10%] flex flex-col space-y-5"
+        onSubmit={handleSubmit(onSubmit)}>
+        <Icon id="arrow-back" />
+        <div style={{ textAlign: "center" }}>
+          <p>
+            {currentUser}님, <br />
+            {auction.sellerInfo.nickname}님과의 거래는 어떠셨나요?
+          </p>
+        </div>
+        <ProductCard id={1}>
+          <div className="flex w-full border-t py-4">
+            <ProductCard.CardImage
+              titleImage={auction.imageUrls[0]}
+              width={100}
+              height={100}
+              className="group-hover:[&_img]:scale-100"
+            />
+            <div className="flex grow justify-between flex-col pl-2">
+              <div>
+                <ProductCard.CardTitle className="text-lg w-[180px] overflow-hidden whitespace-nowrap text-ellipsis">
+                  {auction.title}
+                </ProductCard.CardTitle>
 
-              <p className="text-sm">
-                최종낙찰가:
-                <strong>{auction.currentBiddingPrice}원</strong>
-              </p>
-            </div>
-            <div className="self-end">
-              <p className="text-xs text-[#ABABAB] text-right">
-                {formatReadableDate(auction.createdAt)}
-              </p>
+                <p className="text-sm">
+                  최종낙찰가:
+                  <strong>{auction.currentBiddingPrice}원</strong>
+                </p>
+              </div>
+              <div className="self-end">
+                <p className="text-xs text-[#ABABAB] text-right">
+                  {formatReadableDate(auction.createdAt)}
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      </ProductCard>
+        </ProductCard>
 
-      <SelectRange onSelected={handleRangeSelected} />
-      <SelectReview onSelected={handleReviewSelected} />
-      <div>
-        <p>남기고 싶은 후기가 있다면 적어주세요</p>
-        <Controller
-          control={control}
-          name="content"
-          defaultValue=""
-          render={({ field }) => (
-            <textarea
-              {...field}
-              style={{
-                backgroundColor: "#f8f8f8",
-                scrollbarColor: "#96E4FF #ffffff",
-                scrollbarWidth: "thin"
-              }}
-              className="w-full h-[150px] resize-none border-2 rounded-xl border-[#efefef] p-2 text-black"
-            />
-          )}
-        />
-        <ErrorMessage
-          errors={errors}
-          name="content"
-          render={({ message }) => (
-            <p className="text-red-600 mt-2">{message}</p>
-          )}
-        />
-      </div>
-      <Button color="primary">후기 작성 완료하기</Button>
-    </form>
+        <SelectRange onSelected={handleRangeSelected} />
+        <SelectReview onSelected={handleReviewSelected} />
+        <div>
+          <p>남기고 싶은 후기가 있다면 적어주세요</p>
+          <Controller
+            control={control}
+            name="content"
+            defaultValue=""
+            render={({ field }) => (
+              <textarea
+                {...field}
+                style={{
+                  backgroundColor: "#f8f8f8",
+                  scrollbarColor: "#96E4FF #ffffff",
+                  scrollbarWidth: "thin"
+                }}
+                className="w-full h-[150px] resize-none border-2 rounded-xl border-[#efefef] p-2 text-black"
+              />
+            )}
+          />
+          <ErrorMessage
+            errors={errors}
+            name="content"
+            render={({ message }) => (
+              <p className="text-red-600 mt-2">{message}</p>
+            )}
+          />
+        </div>
+        <Button color="primary">후기 작성 완료하기</Button>
+      </form>
+      {review && (
+        <Modal
+          className="dark:bg-[#181818]"
+          modalType="fullScreen"
+          animate="slide"
+          isOpen={isOpen}
+          close={close}>
+          <ReviewReceipt review={review && review} />
+        </Modal>
+      )}
+    </>
   );
 };
 
