e027d34a34bd13602faa2f705ae330b646a612a9
JangGwon <tlsakch510@naver.com>
2024-03-29 23:16:54 +0900

    feat : submit버튼에 클릭 이벤트 추가

--- src/app/_component/common/Input/SubmitButton.tsx
+++ src/app/_component/common/Input/SubmitButton.tsx
@@ -1,13 +1,16 @@
 import { VariantProps } from "class-variance-authority";
 import { ComponentPropsWithoutRef, PropsWithChildren } from "react";
+import { useContext } from "react";
 
 import { cn } from "@/utils/function/cn";
 
+import { InputContext } from ".";
 import { SubmitButtonVariants } from "./SubmitButton.variants";
 
 interface SubmitButtonProps
   extends VariantProps<typeof SubmitButtonVariants>,
     Omit<ComponentPropsWithoutRef<"button">, "color"> {
+  onButtonClick: (inputText: string) => void;
   className?: string;
   buttonText?: string;
 }
@@ -16,19 +19,23 @@ const SubmitButton = ({
   className,
   children,
   buttonText,
+  onButtonClick,
   color,
   size,
   fontSize,
   ...props
 }: PropsWithChildren<SubmitButtonProps>) => {
+  const { inputText } = useContext(InputContext);
   return children ? (
     <button
+      onClick={() => onButtonClick(inputText)}
       {...props}
       className={className}>
       {children}
     </button>
   ) : (
     <button
+      onClick={() => onButtonClick(inputText)}
       {...props}
       className={cn(
         SubmitButtonVariants({ color, size, fontSize }),
