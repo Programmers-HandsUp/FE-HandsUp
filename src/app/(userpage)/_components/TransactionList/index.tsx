0f4d3fbae7ccb69dc81bfa0098bb24cbfe9bcfce
imssein <hwinnie131@gmail.com>
2024-04-01 13:27:42 +0900

    design: 불필요한 css 삭제

--- src/app/(userpage)/_components/TransactionList/index.tsx
+++ src/app/(userpage)/_components/TransactionList/index.tsx
@@ -7,7 +7,7 @@ import { StatusEn } from "@/utils/types/user/mypage";
 import usePurchaseStatusList from "../../_hooks/queries/usePurchaseStatusList";
 import useSaleStatusList from "../../_hooks/queries/useSaleStatusList";
 import PurchaseHistory from "../PurchaseHistory";
-import SalesHistory from "../SalesHistory/SalesHistory";
+import SalesHistory from "../SalesHistory";
 import Tab from "../Tab";
 
 export type TradeMethod = "구매" | "판매";
@@ -25,7 +25,7 @@ function TransactionList({ tradeMethod, userId }: TransactionListProps) {
 
   return (
     <>
-      <div className="w-[360px] z-10 backdrop-blur-md opacity-80">
+      <div className="z-10 backdrop-blur-md opacity-80">
         <div className="grid grid-cols-3 items-center h-14 border-b">
           <Tab
             status="BIDDING"
