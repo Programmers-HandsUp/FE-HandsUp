09f92423b452edf216f6eb26c25cd46e0ac5ac9a
imssein <hwinnie131@gmail.com>
2024-04-09 15:58:19 +0900

    refactor: 상태 관련 컴포넌트 렌더링 로직 분리

--- src/app/(userpage)/_components/SalesHistory/index.tsx
+++ src/app/(userpage)/_components/SalesHistory/index.tsx
@@ -1,48 +1,18 @@
-import { ReactNode } from "react";
-
-import Timer from "@/app/_component/common/Timer";
 import { SaleContent, StatusEn } from "@/utils/types/user/mypage";
 
 import EmptyList from "../EmptyList";
 import Transaction from "../Transaction/Transaction";
-import { HeaderProps } from "../Transaction/TransactionItem";
+import { statusDate, statusHeader } from "./StatusComponents";
 
 interface SalesHistoryProps {
   status: StatusEn;
   saleList: SaleContent[];
 }
 
-const statusHeader: Record<StatusEn, HeaderProps> = {
-  BIDDING: {
-    firstLabel: "최고 입찰가",
-    secondLabel: "남은 시간"
-  },
-  TRADING: {
-    firstLabel: "구매 희망가",
-    secondLabel: "-"
-  },
-  COMPLETED: {
-    firstLabel: "낙찰가",
-    secondLabel: "날짜"
-  }
-};
-
 function SalesHistory({ saleList, status }: SalesHistoryProps) {
   if (saleList.length === 0) {
     return <EmptyList type="판매" />;
   }
-
-  const statusDate: Record<StatusEn, (sale: SaleContent) => ReactNode> = {
-    BIDDING: (sale) => (
-      <Timer
-        isIcon={false}
-        createdAt={sale.auctionCreatedAt}
-        deadline={sale.auctionEndDateTime}
-      />
-    ),
-    TRADING: () => <Transaction.Date />,
-    COMPLETED: (sale) => <Transaction.Date date={sale.auctionCreatedAt} />
-  };
 
   return (
     <>
