0a996fb606885b9c56d51fdab374c70b92c41abb
JangGwon <tlsakch510@naver.com>
2024-07-30 17:56:21 +0900

    test : 모킹 데이터(postlistData) 변경

--- src/utils/mocks/api/data/postListData.ts
+++ src/utils/mocks/api/data/postListData.ts
@@ -49,13 +49,13 @@ export const auctionHotList: RecommendAuctionsResponse = {
 
 export const auctionDetail = [
   {
-    biddingCount: 0,
+    biddingCount: 1,
     auctionId: 353256236,
-    title: "나이키 덩크 로우 팔아영",
+    title: "(mock)모킹1",
     productCategory: "신발",
     initPrice: 5000,
     currentBiddingPrice: 12000,
-    endDate: "2024-03-12",
+    endDate: "2024-08-04",
     productStatus: "깨끗해요",
     purchaseTime: "3개월 이하",
     auctionStatus: "입찰 중",
@@ -63,15 +63,46 @@ export const auctionDetail = [
       "2개월 정도 신었나? 잘 인신게 돼서 경매에 올려용. 상태는 깨끗합니다!",
     tradeMethod: "직접 만나서",
     imageUrls: [
-      "https://static.shoeprize.com/uploads/2022-12-30/defc77d1-320d-46ff-b29c-617cde450d3b.webp",
-      "https://static.shoeprize.com/uploads/2022-12-30/defc77d1-320d-46ff-b29c-617cde450d3b.webp",
-      "https://static.shoeprize.com/uploads/2022-12-30/defc77d1-320d-46ff-b29c-617cde450d3b.webp"
+      "~/images/angel.webp",
+      "~/images/angel.webp",
+      "~/images/angel.webp"
     ],
     sellerInfo: {
       userId: 123,
       nickname: "안뇽",
-      profileImageUrl:
-        "https://static.shoeprize.com/uploads/2022-12-30/defc77d1-320d-46ff-b29c-617cde450d3b.webp",
+      profileImageUrl: "~/images/angel.webp",
+      dong: "강남동",
+      score: 150
+    },
+    si: "서울시",
+    gu: "강남구",
+    dong: "논현동",
+    bookmarkCount: 0,
+    createdAt: "2024-02-12"
+  },
+  {
+    biddingCount: 1,
+    auctionId: 353256236,
+    title: "(mock)모킹2",
+    productCategory: "신발",
+    initPrice: 5000,
+    currentBiddingPrice: 16000,
+    endDate: "2024-08-12",
+    productStatus: "깨끗해요",
+    purchaseTime: "3개월 이하",
+    auctionStatus: "입찰 중",
+    description:
+      "2개월 정도 신었나? 잘 인신게 돼서 경매에 올려용. 상태는 깨끗합니다!",
+    tradeMethod: "직접 만나서",
+    imageUrls: [
+      "~/images/angel.webp",
+      "~/images/angel.webp",
+      "~/images/angel.webp"
+    ],
+    sellerInfo: {
+      userId: 123,
+      nickname: "김김",
+      profileImageUrl: "~/images/angel.webp",
       dong: "강남동",
       score: 150
     },
