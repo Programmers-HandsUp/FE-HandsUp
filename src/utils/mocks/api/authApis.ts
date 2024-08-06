11811341aeea18b06366bc7a5496e3df76f0dec6
JangGwon <tlsakch510@naver.com>
2024-08-06 18:59:17 +0900

    test : 이메일 중복검사 모킹 api 추가

--- src/utils/mocks/api/authApis.ts
+++ src/utils/mocks/api/authApis.ts
@@ -32,21 +32,29 @@ const handler = [
       throw new Error(`${error}`);
     }
   }),
-  http.get("/api/idduplicatecheck/:id", async ({ params }) => {
-    try {
-      const { id } = params;
-      if (!id || typeof id !== "string") {
-        throw new Error("아이디 데이터가 없습니다.");
-      }
-      if (userAuthData.filter((authItem) => authItem.email === id).length) {
+  http.get(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/users/check-email`,
+    async ({ request }) => {
+      const { searchParams } = new URL(request.url);
+
+      const email = searchParams.get("email") || 0;
+      try {
+        if (!email || typeof email !== "string") {
+          throw new Error("아이디 데이터가 없습니다.");
+        }
+
+        if (userAuthData.find((authItem) => authItem.email === email)) {
+          return new HttpResponse("이미 사용중인 이메일 입니다.", {
+            status: 401
+          });
+        } else {
+          return new HttpResponse(null, { status: 200 });
+        }
+      } catch (error) {
         return new HttpResponse(null, { status: 401 });
-      } else {
-        return new HttpResponse(null, { status: 200 });
       }
-    } catch (error) {
-      return new HttpResponse(null, { status: 401 });
     }
-  }),
+  ),
   http.post(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/login`,
     async ({ request }) => {
