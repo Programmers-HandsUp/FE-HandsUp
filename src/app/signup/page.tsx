d49eaf21dd3200fe1e21145997233c0665f02726
JangGwon <tlsakch510@naver.com>
2024-08-09 00:45:25 +0900

    refactor : signupHeader 파일 경로 수정

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -12,7 +12,7 @@ import ProfileImageForm from "./_component/funnel/ProfileImageForm";
 import SelectCategoryForm from "./_component/funnel/SelectCategoryForm";
 import SelectResidenceForm from "./_component/funnel/SelectResidenceForm";
 import SignUpFinishPage from "./_component/funnel/SignUpFinishPage";
-import SignUpHeader from "./_component/SignUpHeader";
+import SignUpHeader from "./_component/SignupHeader";
 import { SignUpFormSchema } from "./utils/validation";
 
 const SignUp = () => {
