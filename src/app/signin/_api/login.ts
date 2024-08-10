f0eb263d1546e31a997945e51e36b42b8ea176f7
JangGwon <tlsakch510@naver.com>
2024-08-11 02:12:19 +0900

    fix : login APi에서 로그인 성공시 토큰 쿠키에 저장

--- src/app/signin/_api/login.ts
+++ src/app/signin/_api/login.ts
@@ -1,3 +1,4 @@
+import { setCookie } from "@/utils/function/cookie";
 import { LoginRequest } from "@/utils/types/authorization/login";
 
 export const signIn = async (authData: LoginRequest): Promise<boolean> => {
@@ -11,7 +12,9 @@ export const signIn = async (authData: LoginRequest): Promise<boolean> => {
       body: JSON.stringify(authData)
     }
   );
+  const result = await response.json();
   if (response.ok) {
+    setCookie({ name: "accessToken", value: result.accessToken });
     return true;
   }
   throw new Error(response.status.toString());
