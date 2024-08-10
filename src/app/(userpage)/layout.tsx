5b189bf2c7ef98f67f11f68195dec87f7dd7dd9e
JangGwon <tlsakch510@naver.com>
2024-08-10 17:02:39 +0900

    refactor : UserPage의 프리패치 쿼리 제거

--- src/app/(userpage)/layout.tsx
+++ src/app/(userpage)/layout.tsx
@@ -1,8 +1,3 @@
-import { QueryClient } from "@tanstack/react-query";
-
-import { CheckLoginUserResponse } from "@/utils/types/user/users";
-
-import { getLoginUserInfo } from "../_api/user";
 import Navigation from "../_component/common/Navigation";
 
 export default async function UserPageLayout({
@@ -10,15 +5,6 @@ export default async function UserPageLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: ["user"],
-    queryFn: getLoginUserInfo
-  });
-
-  const user = queryClient.getQueryData<CheckLoginUserResponse>(["user"]);
-
   return (
     <section>
       <div className="px-2">{children}</div>
