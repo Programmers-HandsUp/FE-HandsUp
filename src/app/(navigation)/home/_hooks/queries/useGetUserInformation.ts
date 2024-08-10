952a3d2799ef84e15bf795f38dfb901f8d302b05
JangGwon <tlsakch510@naver.com>
2024-08-10 18:30:34 +0900

    refactor : useGetUserInformation훅 기존 UseQuery 사용방식에서 useSuspenseQuery 사용방식으로 변경

--- src/app/(navigation)/home/_hooks/queries/useGetUserInformation.ts
+++ src/app/(navigation)/home/_hooks/queries/useGetUserInformation.ts
@@ -1,9 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 
 import { getLoginUserInfo } from "@/app/_api/user";
 
 const useUserInformation = () => {
-  return useQuery({
+  return useSuspenseQuery({
     queryKey: ["userInformation"],
     queryFn: getLoginUserInfo
   });
