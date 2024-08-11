a4cac176b7064c502fb8890de44d8576d613ad6d
JangGwon <tlsakch510@naver.com>
2024-08-11 16:32:09 +0900

    fix : 로그인 모킹api 반환물에 엑세스 토큰 추가

--- src/utils/mocks/api/authApis.ts
+++ src/utils/mocks/api/authApis.ts
@@ -76,13 +76,16 @@ const handler = [
           authRequestInform.password === authItem.password
       );
       if (isUser) {
-        return new HttpResponse(null, {
-          headers: {
-            "Content-Type": "application/json",
-            "Set-Cookie": `AccessToken=${mockTokens.accessToken}`
-          },
-          status: 200
-        });
+        return new HttpResponse(
+          JSON.stringify({ AccessToken: mockTokens.accessToken }),
+          {
+            headers: {
+              "Content-Type": "application/json",
+              "Set-Cookie": `AccessToken=${mockTokens.accessToken}`
+            },
+            status: 200
+          }
+        );
       }
       return new HttpResponse(null, {
         status: 401
